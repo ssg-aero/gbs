be3e241c79a800c8e8a81b092d62a7c81af449e4
Sébastien Raymond <sebastien.newe@gmail.com>
2020-08-25 09:20:19 +0200

    Build approximation curve

--- gbslib/bscapprox.h
+++ gbslib/bscapprox.h
@@ -5,23 +5,90 @@
 
 namespace gbs
 {
-    template <typename T, size_t dim>
-    auto approx(const std::vector<std::array<T, dim>> &pts, size_t p, size_t n_poles, const std::vector<T> &u) -> gbs::BSCurve<T,dim>
+    template <typename T>
+    void removeRow(Eigen::MatrixX<T> &matrix, unsigned int rowToRemove)
     {
-        // auto k_flat = build_simple_mult_flat_knots<T>(u, n_poles, p);
-        auto nk = n_poles + p + 1;
-        std::vector<double> k_flat(nk);
-        std::fill(k_flat.begin(), std::next(k_flat.begin(), p), 0.);
-        std::fill(std::next(k_flat.begin(), nk - 1 - p), k_flat.end(), u.back() - u.front());
-        for (int j = 1; j < n_poles - p; j++) // TODO use std algo
+        unsigned int numRows = matrix.rows() - 1;
+        unsigned int numCols = matrix.cols();
+
+        if (rowToRemove < numRows)
+            matrix.block(rowToRemove, 0, numRows - rowToRemove, numCols) = matrix.bottomRows(numRows - rowToRemove);
+
+        matrix.conservativeResize(numRows, numCols);
+    }
+
+    template <typename T>
+    void removeColumn(Eigen::MatrixX<T> &matrix, unsigned int colToRemove)
+    {
+        unsigned int numRows = matrix.rows();
+        unsigned int numCols = matrix.cols() - 1;
+
+        if (colToRemove < numCols)
+            matrix.block(0, colToRemove, numRows, numCols - colToRemove) = matrix.rightCols(numCols - colToRemove);
+
+        matrix.conservativeResize(numRows, numCols);
+    }
+
+    template <typename T, size_t dim>
+    auto approx_bound_fixed(const std::vector<std::array<T, dim>> &pts, size_t p, size_t n_poles, const std::vector<T> &u, std::vector<double> k_flat) -> gbs::BSCurve<T, dim>
+    {
+        auto n_params = int(u.size());
+        Eigen::MatrixX<T> N(n_params-2, n_poles-2);
+
+
+        for (int i = 0; i < n_params-2; i++)
         {
-            k_flat[j + p] = j / double(n_poles - p);
+            for (int j = 0; j < n_poles-2; j++)
+            {
+                    N(i , j) = gbs::basis_function(u[i+1], j+1, p, 0, k_flat);
+            }
         }
 
+        auto p_begin = pts.front();
+        auto p_end = pts.back();
+        std::vector<T> Nbegin(n_params-2),Nend(n_params-2);
+        for (int i = 0; i < n_params-2; i++)
+        {
+            Nbegin[i] = gbs::basis_function(u[i+1], 0, p, 0, k_flat);
+            Nend[i] = gbs::basis_function(u[i+1], n_poles-1, p, 0, k_flat);
+        }
+
+
+        // auto N_inv = N.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV);
+        auto N_inv = N.colPivHouseholderQr();
+
+        auto n_pt = pts.size();
+
+        std::vector<std::array<T, dim>> poles(n_poles);
+        Eigen::VectorX<T> b(n_pt-2);
+        for (int d = 0; d < dim; d++)
+        {
+            for (int i = 0; i < n_pt-2; i++)
+            {
+                b(i) = pts[i+1][d] - p_begin[d] * Nbegin[i] - p_end[d] * Nend[i]; //Pas top au niveau de la localisation mémoire
+            }
+
+            auto x = N_inv.solve(b);
+
+            for (int i = 1; i < n_poles - 1; i++)
+            {
+                poles[i][d] = x(i - 1);
+            }
+        }
+        poles.front() = pts.front();
+        poles.back() = pts.back();
+
+        return BSCurve(poles, k_flat, p);
+    }
+
+    template <typename T, size_t dim>
+    auto approx(const std::vector<std::array<T, dim>> &pts, size_t p, size_t n_poles, const std::vector<T> &u, std::vector<double> k_flat) -> gbs::BSCurve<T, dim>
+    {
         Eigen::MatrixX<T> N(u.size(), n_poles);
         build_poles_matix<T, 1>(k_flat, u, p, n_poles, N);
 
-        auto N_inv = N.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV);
+        // auto N_inv = N.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV);
+        auto N_inv = N.colPivHouseholderQr();
 
         auto n_pt = pts.size();
 
@@ -31,7 +98,7 @@ namespace gbs
         {
             for (int i = 0; i < n_pt; i++)
             {
-                    b( i ) = pts[i][d]; //Pas top au niveau de la localisation mémoire
+                b(i) = pts[i][d]; //Pas top au niveau de la localisation mémoire
             }
 
             auto x = N_inv.solve(b);
@@ -42,21 +109,98 @@ namespace gbs
             }
         }
 
-        return BSCurve(poles,k_flat,p);
+        return BSCurve(poles, k_flat, p);
     }
 
     template <typename T, size_t dim>
-    auto approx(const std::vector<std::array<T, dim>> &pts, size_t p,const std::vector<T> &u) // -> gbs::BSCurve<T,dim>
+    auto approx(const std::vector<std::array<T, dim>> &pts, size_t p, size_t n_poles, const std::vector<T> &u, bool fix_bound) -> gbs::BSCurve<T, dim>
     {
-        // auto k_flat = build_simple_mult_flat_knots<T>(u,p,p); // init avec mini
+        auto nk = n_poles + p + 1;
+        std::vector<double> k_flat(nk);
+        std::fill(k_flat.begin(), std::next(k_flat.begin(), p), 0.);
+        std::fill(std::next(k_flat.begin(), nk - 1 - p), k_flat.end(), u.back() - u.front());
+        for (int j = 1; j < n_poles - p; j++) // TODO use std algo
+        {
+            k_flat[j + p] = j / double(n_poles - p);
+        }
 
+        if (fix_bound)
+        {
+            return approx_bound_fixed(pts, p, n_poles, u, k_flat);
+        }
+        else
+        {
+            return approx(pts, p, n_poles, u, k_flat);
+        }
     }
 
     template <typename T, size_t dim>
-    auto approx(const std::vector<std::array<T, dim>> &pts, size_t p, gbs::KnotsCalcMode mode ) // -> gbs::BSCurve<T,dim>
+    auto approx(const std::vector<std::array<T, dim>> &pts, size_t p, gbs::KnotsCalcMode mode, bool fix_bound) -> gbs::BSCurve<T, dim>
     {
-        auto u = gbs::curve_parametrization(pts, gbs::KnotsCalcMode::CHORD_LENGTH, true);
-        return approx(pts,p,u);
+        auto u = gbs::curve_parametrization(pts, mode, true);
+        auto n_poles = p * 2;
+        // auto n_poles = pts.size() / 5;
+        auto crv = approx(pts, p, n_poles, u, fix_bound);
+
+        for (int i = 0; i < 200; i++)
+        {
+            auto d_avg = 0., d_max = 0., u_max = -1.;
+            std::vector<T> knots{crv.knotsFlats()};
+            auto u0 = knots.front();
+            std::for_each(
+                pts.begin(),
+                pts.end(),
+                [&](const auto &pnt) {
+                    auto res = gbs::extrema_PC(crv, pnt, u0, 1e-6);
+                    u0 = res.u;
+                    auto it = std::lower_bound(knots.begin(), knots.end(), res.u);
+                    auto uh = *it;
+                    if (it == knots.begin())
+                        it = std::next(it);
+                    auto ul = *(std::next(it, -1));
+                    auto dul = (res.u - ul) / (uh - ul);
+                    auto duh = (uh - res.u) / (uh - ul);
+                    if (
+                        res.d > d_max 
+                        && dul > 0.33 
+                        && duh > 0.33
+                        )
+                    {
+                        d_max = res.d;
+                        u_max = res.u;
+                    }
+                    d_avg += res.d;
+                });
+
+            auto it = std::lower_bound(knots.begin(), knots.end(), u_max);
+            knots.insert(it, u_max);
+            n_poles++;
+            if (fix_bound)
+            {
+                crv = approx_bound_fixed(pts, p, n_poles, u, knots);
+            }
+            else
+            {
+                crv = approx(pts, p, n_poles, u, knots);
+            }
+            d_avg /= pts.size();
+            std::cout << "d_avg: " << d_avg << ", d_max: " << d_max << ", u_max:" << u_max << std::endl;
+            if (
+                u_max < 0 
+                || d_max < 1e-3
+                || d_avg < 1e-4
+                )
+                break;
+            std::cout << "n poles: " << crv.poles().size() << ", n_flat: " << crv.knotsFlats().size() << std::endl;
+        }
+        return crv;
     }
 
-}
+    template <typename T, size_t dim>
+    auto approx(const std::vector<std::array<T, dim>> &pts, size_t p, size_t n_poles, gbs::KnotsCalcMode mode) // -> gbs::BSCurve<T,dim>
+    {
+        auto u = gbs::curve_parametrization(pts, mode, true);
+        return approx(pts, p, n_poles, u, true);
+    }
+
+} // namespace gbs
--- tests/tests_bsinterp.cpp
+++ tests/tests_bsinterp.cpp
@@ -3,6 +3,8 @@
 #include <gbslib/bscapprox.h>
 #include <gbslib/knotsfunctions.h>
 #include <gbslib/bscurve.h>
+#include <gbslib/extrema.h>
+#include <gbslib/bscanalysis.h>
 #include <occt-utils/export.h>
 #include <occt-utils/curvesbuild.h>
 
@@ -10,7 +12,12 @@
 #include <iostream>
 #include <fstream>
 
+#include <GeomTools.hxx>
+#include <BRepBuilderAPI_MakeVertex.hxx>
+
 const double tol = 1e-10;
+
+using gbs::operator-;
 
 template <typename T, size_t dim, size_t nc>
 inline void test_crv(const std::vector<gbs::constrType<T, dim, nc>> Q, const gbs::BSCurve<T, dim> &c, const std::vector<T> &u)
@@ -27,18 +34,19 @@ inline void test_crv(const std::vector<gbs::constrType<T, dim, nc>> Q, const gbs
 }
 
 template <typename T, size_t dim, size_t nc>
-inline bool test_crv(const std::vector<gbs::constrType<T, dim, nc>> Q, const gbs::BSCurve<T, dim> &c, gbs::KnotsCalcMode mode,bool adim=false)
+inline bool test_crv(const std::vector<gbs::constrType<T, dim, nc>> Q, const gbs::BSCurve<T, dim> &c, gbs::KnotsCalcMode mode, bool adim = false)
 {
     std::vector<std::array<T, dim>> pts(Q.size());
     std::transform(Q.begin(), Q.end(), pts.begin(), [](const auto &q_) { return q_.front(); });
-    auto u = gbs::curve_parametrization(pts, gbs::KnotsCalcMode::CHORD_LENGTH,adim);
+    auto u = gbs::curve_parametrization(pts, gbs::KnotsCalcMode::CHORD_LENGTH, adim);
     for (int i = 0; i < Q.size(); i++)
     {
         for (int j = 0; j < Q[i].size(); j++)
         {
             auto p1 = c.value(u[i], j);
             auto p2 = Q[i][j];
-            if(gbs::distance(p1, p2)> tol) return false;
+            if (gbs::distance(p1, p2) > tol)
+                return false;
         }
     }
     return true;
@@ -55,7 +63,6 @@ TEST(tests_bscurve, interp1)
 
     std::vector<std::array<double, 3>> pts(Q.size());
     std::transform(Q.begin(), Q.end(), pts.begin(), [](const auto &q_) { return q_.front(); });
-
 
     // interp bezier
 
@@ -77,31 +84,30 @@ TEST(tests_bscurve, interp1)
             {{0., 1., 1.}},
             {{0., -1., 4.}},
         };
-    
+
     std::vector<Handle_Geom_Curve> crv_lst;
-    crv_lst.push_back( occt_utils::BSplineCurve(c_bez) );
-    for(size_t p = 1; p < Q.size(); p++)
+    crv_lst.push_back(occt_utils::BSplineCurve(c_bez));
+    for (size_t p = 1; p < Q.size(); p++)
     {
-        auto c = gbs::interpolate(Q,p,gbs::KnotsCalcMode::CHORD_LENGTH);
-        ASSERT_TRUE( test_crv(Q, c, gbs::KnotsCalcMode::CHORD_LENGTH,true));
-        crv_lst.push_back( occt_utils::BSplineCurve(c) );
+        auto c = gbs::interpolate(Q, p, gbs::KnotsCalcMode::CHORD_LENGTH);
+        ASSERT_TRUE(test_crv(Q, c, gbs::KnotsCalcMode::CHORD_LENGTH, true));
+        crv_lst.push_back(occt_utils::BSplineCurve(c));
     }
     // p = 2 produit les courbes les + smmooths
-    occt_utils::to_iges(crv_lst,"tests/out/interp1.igs");
-
+    occt_utils::to_iges(crv_lst, "tests/out/interp1.igs");
 }
 
 TEST(tests_bscurve, interp_C1)
 {
     std::vector<gbs::constrType<double, 3, 2>> Q =
         {
-            {{{0., 0., 0.},{0., 1., 0.}}},
-            {{{1., 0., 0.},{1., 0., 0.}}},
-            {{{1., 1., 0.},{0., 1., 0.}}},
+            {{{0., 0., 0.}, {0., 1., 0.}}},
+            {{{1., 0., 0.}, {1., 0., 0.}}},
+            {{{1., 1., 0.}, {0., 1., 0.}}},
         };
 
     auto mode = gbs::KnotsCalcMode::CHORD_LENGTH;
-    auto c1 = gbs::interpolate(Q,mode);
+    auto c1 = gbs::interpolate(Q, mode);
 
     ASSERT_TRUE(test_crv(Q, c1, mode));
 
@@ -116,96 +122,93 @@ TEST(tests_bscurve, interp_C2)
     // https://stackoverflow.com/questions/49689336/initialize-struct-using-initializer-list-fails-for-stdarray
     std::vector<gbs::constrType<double, 3, 3>> Q =
         {
-            {{{0., 0., 0.},{0., 1., 0.},{1., 0., 0.}}},
-            {{{1., 0., 0.},{1., 0., 0.},{0., 1., 0.}}},
-            {{{1., 0.3, 1.},{1., 0., 0.},{0., 1., 0.}}},
-            {{{1., 1., 0.},{0., 1., 0.},{0., 0., 1.}}},
+            {{{0., 0., 0.}, {0., 1., 0.}, {1., 0., 0.}}},
+            {{{1., 0., 0.}, {1., 0., 0.}, {0., 1., 0.}}},
+            {{{1., 0.3, 1.}, {1., 0., 0.}, {0., 1., 0.}}},
+            {{{1., 1., 0.}, {0., 1., 0.}, {0., 0., 1.}}},
         };
 
     gbs::KnotsCalcMode mode = gbs::KnotsCalcMode::CHORD_LENGTH;
-    ASSERT_TRUE(test_crv(Q, gbs::interpolate(Q,mode), mode));
-
+    ASSERT_TRUE(test_crv(Q, gbs::interpolate(Q, mode), mode));
 
     std::vector<gbs::constrType<double, 2, 3>> Q2d =
         {
-            {{{0., 0.},{0., 1.},{1., 0.}}},
-            {{{1., 0.},{1., 0.},{0., 1.}}},
-            {{{1., 0.3},{1., 0.},{0., 1.}}},
-            {{{1., 1.},{0., 1.},{0.2, 0.}}},
+            {{{0., 0.}, {0., 1.}, {1., 0.}}},
+            {{{1., 0.}, {1., 0.}, {0., 1.}}},
+            {{{1., 0.3}, {1., 0.}, {0., 1.}}},
+            {{{1., 1.}, {0., 1.}, {0.2, 0.}}},
         };
 
     gbs::KnotsCalcMode mode2d = gbs::KnotsCalcMode::CHORD_LENGTH;
-    ASSERT_TRUE(test_crv(Q, gbs::interpolate(Q,mode2d), mode2d));
+    ASSERT_TRUE(test_crv(Q, gbs::interpolate(Q, mode2d), mode2d));
 
     std::vector<gbs::constrType<double, 1, 3>> Q1d =
         {
-            {{{0.},{0.},{1.}}},
-            {{{1.},{1.},{0.}}},
-            {{{1.},{1.},{0.}}},
-            {{{1.},{0.},{0.2}}},
+            {{{0.}, {0.}, {1.}}},
+            {{{1.}, {1.}, {0.}}},
+            {{{1.}, {1.}, {0.}}},
+            {{{1.}, {0.}, {0.2}}},
         };
 
     gbs::KnotsCalcMode mode1d = gbs::KnotsCalcMode::CHORD_LENGTH;
-    ASSERT_TRUE(test_crv(Q, gbs::interpolate(Q,mode1d), mode1d));
+    ASSERT_TRUE(test_crv(Q, gbs::interpolate(Q, mode1d), mode1d));
 
-    std::vector<gbs::constrType<double, 4, 3>> Q4d=
+    std::vector<gbs::constrType<double, 4, 3>> Q4d =
         {
-            {{{0., 0., 0.,1},{0., 1., 0.,0},{1., 0., 0.,0}}},
-            {{{1., 0., 0.,2},{1., 0., 0.,0},{0., 1., 0.,0}}},
-            {{{1., 0.3, 1.,3},{1., 0., 0.,0},{0., 1., 0.,0}}},
-            {{{1., 1., 0.,4},{0., 1., 0.,0},{0., 0., 1.,0}}},
+            {{{0., 0., 0., 1}, {0., 1., 0., 0}, {1., 0., 0., 0}}},
+            {{{1., 0., 0., 2}, {1., 0., 0., 0}, {0., 1., 0., 0}}},
+            {{{1., 0.3, 1., 3}, {1., 0., 0., 0}, {0., 1., 0., 0}}},
+            {{{1., 1., 0., 4}, {0., 1., 0., 0}, {0., 0., 1., 0}}},
         };
     gbs::KnotsCalcMode mode4d = gbs::KnotsCalcMode::CHORD_LENGTH;
-    ASSERT_TRUE(test_crv(Q, gbs::interpolate(Q,mode4d), mode4d));
+    ASSERT_TRUE(test_crv(Q, gbs::interpolate(Q, mode4d), mode4d));
 }
 
 TEST(tests_bscurve, build_3pt_tg)
 {
-    std::vector<std::array<double,3> > pts =
-    {
-        {0.,0.,0.},
-        {0.,1.,0.},
-        {1.,1.,0.},
-        // {1.,1.,1.},
-        // {1.,1.,2.},
-        // {3.,1.,1.},
-        // {0.,4.,1.},
-        {3.,1.,0.},
-        {4.,0.,0.},
-    };
+    std::vector<std::array<double, 3>> pts =
+        {
+            {0., 0., 0.},
+            {0., 1., 0.},
+            {1., 1., 0.},
+            // {1.,1.,1.},
+            // {1.,1.,2.},
+            // {3.,1.,1.},
+            // {0.,4.,1.},
+            {3., 1., 0.},
+            {4., 0., 0.},
+        };
     auto u = gbs::curve_parametrization(pts, gbs::KnotsCalcMode::CHORD_LENGTH);
 
-    auto D = gbs::build_3pt_tg_vec(pts,u);
-    auto T = gbs::build_3pt_tg_dir(pts,u);
+    auto D = gbs::build_3pt_tg_vec(pts, u);
+    auto T = gbs::build_3pt_tg_dir(pts, u);
 
     std::vector<gbs::constrType<double, 3, 2>> Q(pts.size());
     std::transform(
-        pts.begin(),pts.end(),
+        pts.begin(), pts.end(),
         // T.begin(),
         D.begin(),
         Q.begin(),
-        [](const auto &pt_,const auto &tg_)
-        {
-            return gbs::constrType<double, 3, 2>({pt_,tg_});
-        }
-    );
+        [](const auto &pt_, const auto &tg_) {
+            return gbs::constrType<double, 3, 2>({pt_, tg_});
+        });
 
     gbs::KnotsCalcMode mode = gbs::KnotsCalcMode::CHORD_LENGTH;
-    auto crv1 = gbs::interpolate(Q,mode);
-    ASSERT_TRUE(test_crv(Q,crv1, mode));
+    auto crv1 = gbs::interpolate(Q, mode);
+    ASSERT_TRUE(test_crv(Q, crv1, mode));
 
     // gbs::interpolate(Q,p,gbs::KnotsCalcMode::CHORD_LENGTH);
 
     std::vector<Handle_Geom_Geometry> crv_lst;
     crv_lst.push_back(occt_utils::BSplineCurve(crv1));
-    occt_utils::to_iges(crv_lst,"tests/out/build_3pt_tg.igs");
+    occt_utils::to_iges(crv_lst, "tests/out/build_3pt_tg.igs");
 }
 
 TEST(tests_bscurve, approx_simple)
 {
 
     std::string line;
-    std::ifstream myfile ("C:/Users/sebastien/workspace2/gbslib/tests/in/e1098.dat");
+    std::ifstream myfile("C:/Users/sebastien/workspace2/gbslib/tests/in/e1098.dat");
     if (myfile.is_open())
     {
         std::vector<std::array<double, 2>> pts;
@@ -217,17 +220,74 @@ TEST(tests_bscurve, approx_simple)
 
             double x = std::stod(line, &sz);
             double y = std::stod(line.substr(sz));
-            pts.push_back({x,y});
-
-        }        
+            pts.push_back({x, y});
+        }
         myfile.close();
 
-        auto u = gbs::curve_parametrization(pts, gbs::KnotsCalcMode::CHORD_LENGTH, true);
-        auto crv = gbs::approx(pts, 5, 30, u);
+        // auto u = gbs::curve_parametrization(pts, gbs::KnotsCalcMode::CHORD_LENGTH, true);
+        // auto crv = gbs::approx(pts, 5, 10, u);
+        auto crv = gbs::approx(pts, 5, 10, gbs::KnotsCalcMode::CHORD_LENGTH);
 
         std::vector<Handle_Geom2d_Curve> crv_lst;
-        crv_lst.push_back( occt_utils::BSplineCurve(crv));
-        occt_utils::to_iges(crv_lst,"C:/Users/sebastien/workspace2/gbslib/tests/out/approx_simple.igs");
+        crv_lst.push_back(occt_utils::BSplineCurve(crv));
+        occt_utils::to_iges(crv_lst, "C:/Users/sebastien/workspace2/gbslib/tests/out/approx_simple.igs");
+        auto res = gbs::dev_from_points(pts, crv);
+        std::cout << "d_avg: " << res.d_avg << ", d_max:" << res.d_max << ", u_max:" << res.u_max << std::endl;
+    }
+    else
+        std::cout << "Unable to open file";
+}
+
+TEST(tests_bscurve, approx_refined)
+{
+
+    std::string line;
+    std::ifstream myfile("C:/Users/sebastien/workspace2/gbslib/tests/in/e1098.dat");
+    // std::ifstream myfile("C:/Users/sebastien/workspace2/gbslib/tests/in/e817.dat");
+    if (myfile.is_open())
+    {
+        std::vector<std::array<double, 2>> pts;
+        getline(myfile, line);
+        while (getline(myfile, line))
+        {
+            std::istringstream iss(line);
+            std::string::size_type sz; // alias of size_t
+
+            double x = std::stod(line, &sz);
+            double y = std::stod(line.substr(sz));
+            pts.push_back({x, y});
+        }
+        myfile.close();
+
+        auto crv = gbs::approx(pts, 5, gbs::KnotsCalcMode::CHORD_LENGTH,true);
+        std::cout << "n poles: " << crv.poles().size() << ", n_flat: " << crv.knotsFlats().size() <<std::endl;
+
+        std::vector<Handle_Geom2d_Curve> crv_lst;
+        crv_lst.push_back(occt_utils::BSplineCurve(crv));
+        // GeomTools::Dump( occt_utils::BSplineCurve(crv), std::cout );
+        occt_utils::to_iges(crv_lst, "C:/Users/sebastien/workspace2/gbslib/tests/out/approx_refined.igs");
+
+        auto res = gbs::dev_from_points(pts, crv);
+        std::cout << "d_avg: " << res.d_avg << ", d_max:" << res.d_max << ", u_max:" << res.u_max << std::endl;
+
+        auto u0 = crv.knotsFlats().front();
+        std::for_each(
+            pts.begin(),
+            pts.end(),
+            [&](const auto &pnt) {
+                auto res = gbs::extrema_PC(crv, pnt, u0, 1e-6);
+                u0 = res.u;
+                // std::cout << gbs::norm(crv.value(u0)-pnt) << " , " << crv_lst.back()->Value(u0).Distance(occt_utils::point(pnt)) << std::endl;
+            });
+
+        std::vector<TopoDS_Shape> pt_lst(pts.size());
+        std::transform(
+            pts.begin(), pts.end(),
+            pt_lst.begin(),
+            [](const auto &p_) {
+                return BRepBuilderAPI_MakeVertex (gp_Pnt(p_[0],p_[1],0.));
+            });
+        occt_utils::to_iges(pt_lst, "C:/Users/sebastien/workspace2/gbslib/tests/out/foilpoints.igs");
     }
     else
         std::cout << "Unable to open file";
