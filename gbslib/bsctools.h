9530487fc1a4e59b555f6bd4838933cfdf584922
Sébastien Raymond <sebastien.newe@gmail.com>
2020-08-27 12:08:13 +0200

    add trim

---
+++ gbslib/bsctools.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <gbslib/bscurve.h>
+
+namespace gbs
+
+{
+    template <typename T, size_t dim>
+    auto trim_begin(size_t p, std::vector<T> &knots_flats, std::vector<std::array<T, dim>> &poles, T u) -> void
+    {
+        if(u-knots_flats.front()<knot_eps) return;
+        
+        for (auto i = 0; i < p; i++)
+        {
+            insert_knot(u, p, knots_flats, poles);
+        }
+
+        auto it_l = std::lower_bound(knots_flats.begin(), knots_flats.end(), u);
+        auto i_l = it_l - knots_flats.begin();
+        knots_flats.erase(knots_flats.begin(), it_l);
+        knots_flats.insert(knots_flats.begin(), knots_flats.front());
+        poles.erase(poles.begin(), std::next(poles.begin(), i_l - 1));
+
+    }
+
+    template <typename T, size_t dim>
+    auto trim_end(size_t p, std::vector<T> &knots_flats, std::vector<std::array<T, dim>> &poles, T u) -> void
+    {
+        if(knots_flats.back()-u<knot_eps) return;
+        
+        for (auto i = 0; i < p; i++)
+        {
+            insert_knot(u, p, knots_flats, poles);
+        }
+
+        auto it_h = std::lower_bound(knots_flats.begin(), knots_flats.end(), u);
+        auto i_h = it_h - knots_flats.begin();
+        knots_flats.erase(std::next(it_h, p), knots_flats.end());
+        knots_flats.push_back(knots_flats.back());
+        poles.erase(std::next(poles.begin(), i_h), poles.end());
+
+    }
+
+    template <typename T, size_t dim>
+    auto trim(size_t p, std::vector<T> &knots_flats, std::vector<std::array<T, dim>> &poles, T u1, T u2) -> void
+    {
+        trim_begin(p,knots_flats,poles,fmin(u1,u2));
+        trim_end(p,knots_flats,poles,fmax(u1,u2));
+    }
+} // namespace occt_utils
---
+++ tests/tests_bsctools.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+#include <gbslib/bsctools.h>
+
+#include <occt-utils/curvesbuild.h>
+#include <occt-utils/export.h>
+
+TEST(tests_bsctools, trim)
+{
+    std::vector<double> k = {0., 0., 0., 1./4., 1./4., 1./2., 1./2., 3./4., 3./4., 1., 1., 1.};
+    auto wi = sqrt(2.)/2.;
+    auto r = 1.23;
+    std::vector<std::array<double,4> > poles =
+    {
+        { r, 0.,0.,1.},
+        { wi*r, wi*r,0.,wi},
+        { 0., r,0.,1.},
+        {-wi*r, wi*r,0.,wi},
+        {-r, 0.,0.,1.},
+        {-wi*r,-wi*r,0.,wi},
+        { 0.,-r,0.,1.},
+        { wi*r,-wi*r,0.,wi},
+        { r, 0.,0.,1.},
+    };
+
+    size_t p = 2;
+    
+    // gbs::trim(p,k,poles,0.1,3./8.);
+    gbs::trim(p,k,poles,0.,3./8.);
+    gbs::trim(p,k,poles,0.1,3./8.);
+
+    auto c1_3d_dp = gbs::BSCurve<double,4>(poles,k,p);
+
+    std::vector<Handle_Geom_Curve> crv_lst;
+    crv_lst.push_back(occt_utils::NURBSplineCurve(c1_3d_dp));   
+
+    occt_utils::to_iges(crv_lst, "C:/Users/sebastien/workspace2/gbslib/tests/out/trim.igs");
+}
