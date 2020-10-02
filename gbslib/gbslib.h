e3d3bccfac17d377e5cad6d1a55a97b8ebe0063f
szebos <sebastien.newe@gmail.com>
2020-10-02 10:32:01 +0200

    add export macros

--- gbslib/gbslib.h
+++ gbslib/gbslib.h
@@ -1,4 +1,21 @@
 #pragma once
+
+#if defined(_WIN32)
+#ifndef GBS_EXPORT
+#define GBS_EXPORT __declspec( dllexport )
+// For global variables :
+#define GBS_EXPORTEXTERN __declspec( dllexport ) extern
+#define GBS_EXPORTEXTERNC extern "C" __declspec( dllexport )
+#endif  /* GBS_EXPORT */
+
+#ifndef GBS_IMPORT
+#define GBS_IMPORT __declspec( dllimport ) extern
+#define GBS_IMPORTC extern "C" __declspec( dllimport )
+#endif  /* GBS_IMPORT */
+#endif  /*_WIN32 */
+
+#include <vector>
+#include <array>
 namespace gbs
 {
 const double knot_eps = 1e-7;
