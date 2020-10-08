1176857b0b6a1fe1478bb7c07460f0bdc6beec06
szebos <sebastien.newe@gmail.com>
2020-10-08 12:10:45 +0200

    Add point projection on surface

--- gbslib/extrema.h
+++ gbslib/extrema.h
@@ -1,12 +1,20 @@
 #pragma once
 #include <nlopt.hpp>
 #include <gbslib/bscurve.h>
+#include <gbslib/bssurf.h>
 namespace gbs
 {
     template <typename T>
     struct extrema_PC_result
     {
         T u;
+        T d;
+    };
+    template <typename T>
+    struct extrema_PS_result
+    {
+        T u;
+        T v;
         T d;
     };
     /**
@@ -30,7 +38,6 @@ namespace gbs
         public:
             UserData(const gbs::BSCurveGeneral<T, dim, rational> &crv, const std::array<T, dim> &pnt) : p{pnt}
             {
-                // c = const_cast<gbs::BSCurveGeneral<T, dim, rational> *>(&crv);
                 c = &crv;
             }
             const gbs::BSCurveGeneral<T, dim,rational> *c;
@@ -86,5 +93,78 @@ namespace gbs
     {
         auto u0 = 0.5 * (crv.knotsFlats().back() - crv.knotsFlats().front());
         return extrema_PC(crv, pnt, u0,tol_u);
+    }
+    /** Project point on surface
+     * @brief 
+     * 
+     * @tparam T 
+     * @tparam dim 
+     * @tparam rational 
+     * @param srf 
+     * @param pnt 
+     * @param u0 
+     * @param v0 
+     * @param tol_x 
+     * @param solver 
+     * @return extrema_PS_result<T> 
+     */
+    template <typename T, size_t dim, bool rational>
+    auto extrema_PS(const BSSurfaceGeneral<T, dim, rational> &srf, const std::array<T, dim> &pnt, T u0, T v0, T tol_x, const char *solver = "LN_COBYLA") -> extrema_PS_result<T>
+    {
+
+        class UserData
+        {
+        public:
+            UserData(const gbs::BSSurfaceGeneral<T, dim, rational> &srf, const std::array<T, dim> &pnt) : p{pnt}
+            {
+                s = &srf;
+            }
+            const gbs::BSSurfaceGeneral<T, dim, rational> *s;
+            std::array<T, dim> p;
+        };
+        UserData data(srf, pnt);
+
+        auto f = [](const std::vector<T> &x, std::vector<T> &grad, void *user_data) {
+            auto p_d = (UserData *)(user_data);
+            auto c_uv = p_d->s->value(x[0], x[1]);
+            if (!grad.empty())
+            {
+                // auto dc_u = p_d->c->value(x[0], 1);
+                // grad[0] = 0;
+                // for (int i = 0; i < 3; i++)
+                // {
+                //     grad[0] += 2 * dc_u[i] * (c_u[i] - p_d->p[i]);
+                // }
+                throw std::exception("not implemented");
+            }
+            return gbs::sq_norm(c_uv - p_d->p);
+        };
+
+        nlopt::opt opt(solver, 2);
+        std::vector<T> lb(2), hb(2);
+        lb[0] = srf.knotsFlatsU().front();
+        hb[0] = srf.knotsFlatsU().back();
+        lb[1] = srf.knotsFlatsV().front();
+        hb[1] = srf.knotsFlatsV().back();
+
+        opt.set_lower_bounds(lb);
+        opt.set_upper_bounds(hb);
+        opt.set_min_objective(f, &data);
+        opt.set_xtol_rel(tol_x);
+        std::vector<T> x(2);
+        x[0] = u0;
+        x[1] = v0;
+        T minf;
+
+        opt.optimize(x, minf); //can raise
+
+        return {x[0], x[1], sqrt(minf)};
+    }
+        template <typename T, size_t dim, bool rational>
+    auto extrema_PS(const BSSurfaceGeneral<T, dim, rational> &srf, const std::array<T, dim> &pnt, T tol_x, const char *solver = "LN_COBYLA") -> extrema_PS_result<T>
+    {
+        auto u0 = 0.5 * (srf.knotsFlatsU().back() - srf.knotsFlatsU().front());
+        auto v0 = 0.5 * (srf.knotsFlatsV().back() - srf.knotsFlatsV().front());
+        return extrema_PS(srf,pnt,u0,v0,tol_x,solver);
     }
 } // namespace gbs
--- tests/tests_extrema.cpp
+++ tests/tests_extrema.cpp
@@ -26,3 +26,65 @@ TEST(tests_extrema, PC)
     res = gbs::extrema_PC(crv,crv.value(u),1e-6);
     ASSERT_NEAR(res.u,u,1e-6);
 }
+
+TEST(tests_extrema, PS)
+{
+    std::vector<double> ku_flat = {0.,0.,0.,5.,5.,5.};
+    std::vector<double> kv_flat = {0.,0.,0.,1.,2.,3.,3.,3.};
+    size_t p = 2;
+    size_t q = 2;
+
+    //Pij avec j inner loop
+    const std::vector<std::array<double,3> > poles =
+    {
+        {0,0,0},{1,0,1},{1,2,0},
+        {0,1,0},{1,1,1},{1,2,1},
+        {0,2,0},{1,2,1},{1,2,2},
+        {0,3,0},{1,3,1},{1,2,3},
+        {0,4,0},{1,4,1},{1,2,4},
+    };
+
+    gbs::BSSurface srf(poles,ku_flat,kv_flat,p,q);
+    auto u = 2.5;
+    auto v = 1.;
+
+    auto pt = srf.value(u,v);
+
+    auto res = gbs::extrema_PS(srf,pt,1e-6);
+    ASSERT_NEAR(res.u,u,1e-6);
+    ASSERT_NEAR(res.v,v,1e-6);
+
+    std::vector<double> ku = {0.,0.,0.,1.,2.,3.,4.,4.,5.,5.,5.};
+    std::vector<double> kv = {0.,0.,0.,1.,2.,3.,3.,3.};
+
+    std::vector<std::array<double,4> > poles_t = {
+        {0,2,4,1},{0,2,4,1}, {0,6,4,2},    {0,2,0,1},{0,2,0,1},
+        {0,2,4,1},{0,2,4,1}, {0,6,4,2},    {0,2,0,1},{0,2,0,1},
+        {0,2,4,1},{0,2,4,1}, {0,6,4,2},    {0,2,0,1},{0,2,0,1},
+        {0,2,4,1},{4,6,8,2}, {12,24,12,6}, {4,6,0,2},{0,2,0,1},
+        {0,2,4,1},{4,2,4,1}, {8,6,4,2},    {4,2,0,1},{0,2,0,1},
+        {0,2,4,1},{4,2,4,1}, {8,6,4,2},    {4,2,0,1},{0,2,0,1},
+        {0,2,4,1},{4,2,4,1}, {8,6,4,2},    {4,2,0,1},{0,2,0,1},
+        {0,2,4,1},{4,2,4,1}, {8,6,4,2},    {4,2,0,1},{0,2,0,1}
+                                                    };
+
+    //Pij avec i inner loop
+    std::vector<std::array<double,4> > poles_r(poles_t.size());
+    int ni = 5 , nj =8;
+    for (int i = 0; i < ni; i++)
+    {
+        for (int j = 0; j < nj; j++)
+        {
+            poles_r[j + nj * i] = poles_t[i + ni * j];
+        }
+    }
+
+    gbs::BSSurfaceRational<double,3> srfNURBS(poles_r,ku,kv,p,q);
+
+    pt = srfNURBS.value(u,v);
+
+    res = gbs::extrema_PS(srfNURBS,pt,1e-6);
+    ASSERT_NEAR(res.u,u,1e-6);
+    ASSERT_NEAR(res.v,v,1e-6);
+
+}
