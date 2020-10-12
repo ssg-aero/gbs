6755612b12bd26dd80fc60136cf7a013ca3805aa
szebos <sebastien.newe@gmail.com>
2020-10-12 18:30:31 +0200

    use macro to declare templates surves in python

--- python/gbsbind.cpp
+++ python/gbsbind.cpp
@@ -5,64 +5,58 @@
 
 namespace py = pybind11;
 
+// https://stackoverflow.com/questions/47487888/pybind11-template-class-of-many-types
+template <typename T, size_t dim, bool rational>
+void declare_curve(py::module &m)
+{
+       
+        using Class = typename std::conditional<rational, gbs::BSCurveRational<T, dim>, gbs::BSCurve<T, dim>>::type;
+
+        std::string typestr;
+        std::string rationalstr;
+        if(rational) rationalstr="Rational";
+        T x;
+        auto typeid_name = typeid(x).name();
+
+        if     ( strcmp (typeid_name,"float")  == 0 )  typestr = "f";
+        else if( strcmp (typeid_name,"double") == 0 ) typestr = "d";
+        else                                  typestr = typeid_name ;
+
+        std::string pyclass_name = std::string("BSCurve")+ rationalstr + std::to_string(dim) + "d_" + typestr;
+
+        py::class_<Class>(m, pyclass_name.c_str())
+        .def(py::init<
+                const gbs::points_vector<T,dim+rational> &,
+                const std::vector<T> &,
+                size_t 
+                >())
+        .def("value", &Class::value,"Curve evaluation at given parameter",py::arg("u"),py::arg("d") = 0)
+        .def("begin", &Class::begin,"Curve evaluation at begin",py::arg("d") = 0)
+        .def("end", &Class::end,"Curve evaluation at end",py::arg("d") = 0)
+        .def("degree", &Class::degree,"Curve's degree")
+        .def("knotsFlats", &Class::knotsFlats,"Curve's knots")
+        .def("insertKnot", &Class::insertKnot,"Insert knot with the given multiplicity",py::arg("u"),py::arg("m") = 1)
+        .def("removeKnot", &Class::removeKnot,"Try to remove m times the given knot",py::arg("u"),py::arg("tol"),py::arg("m") = 1)
+        .def("poles", &Class::poles,"Curve's poles")
+        .def("reverse", &Class::reverse,"reverse curve orientation")
+        .def("trim", &Class::trim,"Permanently trim curve between u1 and u2 by inserting knots and dropping useless ones")
+        .def("changeBounds", &Class::changeBounds,"Change parametrization to fit between k1 and k2")
+        .def("bounds", &Class::bounds,"Returns curves's start stop values")
+        ;
+}
+
 PYBIND11_MODULE(gbs, m) {
-    py::class_<gbs::BSCurve3d_d>(m, "BSCurve3d_d")
-        .def(py::init<
-                const gbs::points_vector<double,3> &,
-                const std::vector<double> &,
-                size_t 
-                >())
-        .def("value", &gbs::BSCurve3d_d::value,"Curve evaluation at given parameter",py::arg("u"),py::arg("d") = 0)
-        .def("begin", &gbs::BSCurve3d_d::begin,"Curve evaluation at begin",py::arg("d") = 0)
-        .def("end", &gbs::BSCurve3d_d::end,"Curve evaluation at end",py::arg("d") = 0)
-        .def("degree", &gbs::BSCurve3d_d::degree,"Curve's degree")
-        .def("knotsFlats", &gbs::BSCurve3d_d::knotsFlats,"Curve's knots")
-        .def("insertKnot", &gbs::BSCurve3d_d::insertKnot,"Insert knot with the given multiplicity",py::arg("u"),py::arg("m") = 1)
-        .def("removeKnot", &gbs::BSCurve3d_d::removeKnot,"Try to remove m times the given knot",py::arg("u"),py::arg("tol"),py::arg("m") = 1)
-        .def("poles", &gbs::BSCurve3d_d::poles,"Curve's poles")
-        .def("reverse", &gbs::BSCurve3d_d::reverse,"reverse curve orientation")
-        .def("trim", &gbs::BSCurve3d_d::trim,"Permanently trim curve between u1 and u2 by inserting knots and dropping useless ones")
-        .def("changeBounds", &gbs::BSCurve3d_d::changeBounds,"Change parametrization to fit between k1 and k2")
-        .def("bounds", &gbs::BSCurve3d_d::bounds,"Returns curves's start stop values")
-        ;
 
-    py::class_<gbs::BSCurve2d_d>(m, "BSCurve2d_d")
-        .def(py::init<
-                const gbs::points_vector<double,2> &,
-                const std::vector<double> &,
-                size_t 
-                >())
-        .def("value", &gbs::BSCurve2d_d::value,"Curve evaluation at given parameter",py::arg("u"),py::arg("d") = 0)
-        .def("begin", &gbs::BSCurve2d_d::begin,"Curve evaluation at begin",py::arg("d") = 0)
-        .def("end", &gbs::BSCurve2d_d::end,"Curve evaluation at end",py::arg("d") = 0)
-        .def("degree", &gbs::BSCurve2d_d::degree,"Curve's degree")
-        .def("knotsFlats", &gbs::BSCurve2d_d::knotsFlats,"Curve's knots")
-        .def("insertKnot", &gbs::BSCurve2d_d::insertKnot,"Insert knot with the given multiplicity",py::arg("u"),py::arg("m") = 1)
-        .def("removeKnot", &gbs::BSCurve2d_d::removeKnot,"Try to remove m times the given knot",py::arg("u"),py::arg("tol"),py::arg("m") = 1)
-        .def("poles", &gbs::BSCurve2d_d::poles,"Curve's poles")
-        .def("reverse", &gbs::BSCurve2d_d::reverse,"reverse curve orientation")
-        .def("trim", &gbs::BSCurve2d_d::trim,"Permanently trim curve between u1 and u2 by inserting knots and dropping useless ones")
-        .def("changeBounds", &gbs::BSCurve2d_d::changeBounds,"Change parametrization to fit between k1 and k2")
-        .def("bounds", &gbs::BSCurve2d_d::bounds,"Returns curves's start stop values")
-        ;
+        declare_curve<double,3,false>(m);
+        declare_curve<double,2,false>(m);
+        declare_curve<double,1,false>(m);
 
-        py::class_<gbs::BSCurve<double,1>>(m, "BSCurve1d_d")
-        .def(py::init<
-                const gbs::points_vector<double,1> &,
-                const std::vector<double> &,
-                size_t 
-                >())
-        .def("value", &gbs::BSCurve<double,1>::value,"Curve evaluation at given parameter",py::arg("u"),py::arg("d") = 0)
-        .def("begin", &gbs::BSCurve<double,1>::begin,"Curve evaluation at begin",py::arg("d") = 0)
-        .def("end", &gbs::BSCurve<double,1>::end,"Curve evaluation at end",py::arg("d") = 0)
-        .def("degree", &gbs::BSCurve<double,1>::degree,"Curve's degree")
-        .def("knotsFlats", &gbs::BSCurve<double,1>::knotsFlats,"Curve's knots")
-        .def("insertKnot", &gbs::BSCurve<double,1>::insertKnot,"Insert knot with the given multiplicity",py::arg("u"),py::arg("m") = 1)
-        .def("removeKnot", &gbs::BSCurve<double,1>::removeKnot,"Try to remove m times the given knot",py::arg("u"),py::arg("tol"),py::arg("m") = 1)
-        .def("poles", &gbs::BSCurve<double,1>::poles,"Curve's poles")
-        .def("reverse", &gbs::BSCurve<double,1>::reverse,"reverse curve orientation")
-        .def("trim", &gbs::BSCurve<double,1>::trim,"Permanently trim curve between u1 and u2 by inserting knots and dropping useless ones")
-        .def("changeBounds", &gbs::BSCurve<double,1>::changeBounds,"Change parametrization to fit between k1 and k2")
-        .def("bounds", &gbs::BSCurve<double,1>::bounds,"Returns curves's start stop values")
-        ;
+        declare_curve<float,3,false>(m);
+        declare_curve<float,2,false>(m);
+        declare_curve<float,1,false>(m);
+
+        declare_curve<double,3,true>(m);
+        declare_curve<double,2,true>(m);
+        declare_curve<double,1,true>(m);
+
 }
--- python/tests/test_BSCurve.py
+++ python/tests/test_BSCurve.py
@@ -39,3 +39,63 @@ class TestBSCurve(unittest.TestCase):
         x = crv.value(0.5)
 
         self.assertAlmostEqual(x[0],0.5,tol)
+        # simple precision
+        crv = gbs.BSCurve3d_f(
+            [[0.,0.,0.],[1.,0.,0.]],
+            [0.,0.,1.,1.],
+            1)
+
+        x = crv.value(0.5)
+
+        self.assertAlmostEqual(x[0],0.5,tol)
+        self.assertAlmostEqual(x[1],0.,tol)
+        self.assertAlmostEqual(x[2],0.,tol)
+
+        crv = gbs.BSCurve2d_f(
+            [[0.,0.],[1.,0.]],
+            [0.,0.,1.,1.],
+            1)
+
+        x = crv.value(0.5)
+
+        self.assertAlmostEqual(x[0],0.5,tol)
+        self.assertAlmostEqual(x[1],0.,tol)
+
+        crv = gbs.BSCurve1d_f(
+            [[0.],[1.]],
+            [0.,0.,1.,1.],
+            1)
+
+        x = crv.value(0.5)
+
+        self.assertAlmostEqual(x[0],0.5,tol)
+
+        # Rational
+        crv = gbs.BSCurveRational3d_d(
+            [[0.,0.,0.,1.],[1.,0.,0.,1.]],
+            [0.,0.,1.,1.],
+            1)
+
+        x = crv.value(0.5)
+
+        self.assertAlmostEqual(x[0],0.5,tol)
+        self.assertAlmostEqual(x[1],0.,tol)
+        self.assertAlmostEqual(x[2],0.,tol)
+
+        crv = gbs.BSCurve2d_d(
+            [[0.,0.],[1.,0.]],
+            [0.,0.,1.,1.],
+            1)
+
+        x = crv.value(0.5)
+
+        self.assertAlmostEqual(x[0],0.5,tol)
+        self.assertAlmostEqual(x[1],0.,tol)
+
+        crv = gbs.BSCurve1d_d(
+            [[0.],[1.]],
+            [0.,0.,1.,1.],
+            1)
+
+        x = crv.value(0.5)
+        
