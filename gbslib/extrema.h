c82d7cf9d83ee823eff6c197eab746cb40d03060
szebos <sebastien.newe@gmail.com>
2020-10-13 14:41:51 +0200

    Type error fix

--- gbslib/extrema.h
+++ gbslib/extrema.h
@@ -53,7 +53,7 @@ namespace gbs
         };
         UserData data(crv, pnt);
 
-        auto f = [](const std::vector<T> &x, std::vector<T> &grad, void *user_data) {
+        auto f = [](const std::vector<double> &x, std::vector<double> &grad, void *user_data) {
             auto p_d = (UserData *)(user_data);
             auto c_u = p_d->c->value(x[0]);
             if (!grad.empty())
@@ -65,11 +65,11 @@ namespace gbs
                     grad[0] += 2 * dc_u[i] * (c_u[i] - p_d->p[i]);
                 }
             }
-            return gbs::sq_norm(c_u - p_d->p);
+            return double(gbs::sq_norm(c_u - p_d->p));
         };
 
         nlopt::opt opt(solver, 1);
-        std::vector<T> lb(1), hb(1);
+        std::vector<double> lb(1), hb(1);
         lb[0] = crv.bounds()[0];
         hb[0] = crv.bounds()[1];
 
@@ -77,13 +77,13 @@ namespace gbs
         opt.set_upper_bounds(hb);
         opt.set_min_objective(f, &data);
         opt.set_xtol_rel(tol_x);
-        std::vector<T> x(1);
+        std::vector<double> x(1);
         x[0] = u0;
-        T minf;
+        double minf;
 
         opt.optimize(x, minf); //can raise
 
-        return {x[0],sqrt(minf)};
+        return {T(x[0]),T(sqrt(minf))};
 
     }
     /**
@@ -99,7 +99,7 @@ namespace gbs
     template <typename T, size_t dim>
     auto extrema_PC(const Curve<T, dim> &crv, const std::array<T, dim> &pnt,T tol_u) -> extrema_PC_result<T>
     {
-        auto u0 = 0.5 * (crv.bounds()[1] - crv.bounds()[0]);
+        auto u0 = T(0.5) * (crv.bounds()[1] - crv.bounds()[0]);
         return extrema_PC(crv, pnt, u0,tol_u);
     }
     /** Project point on surface
