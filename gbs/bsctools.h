419dd37e29c6da950619aa68c705389c65a30dc4
szebos <sebastien.newe@gmail.com>
2020-10-26 10:47:42 +0100

    add curve unification to prepare surface creation.

--- gbs/bsctools.h
+++ gbs/bsctools.h
@@ -75,4 +75,71 @@ namespace gbs
         trim_begin(p,knots_flats,poles,fmin(u1,u2));
         trim_end(p,knots_flats,poles,fmax(u1,u2));
     }
+    /**
+     * @brief Set all bspline curves to the same degree
+     * 
+     * @tparam Container 
+     * @param bs_lst 
+     */
+    template <typename Container>
+    auto unify_degree(Container &bs_lst) -> void
+    {
+        auto &C = bs_lst.front();
+        // Set first curve at maximun degree
+        std::for_each(
+            std::next(bs_lst.begin()),
+            bs_lst.end(),
+            [&](auto &C_) {
+                while (C.degree()< C_.degree())
+                {
+                    C.increaseDegree();
+                } 
+            });
+        // Set the others at first curve's degree
+        std::for_each(
+            std::next(bs_lst.begin()),
+            bs_lst.end(),
+            [&](auto &C_) {
+                while (C_.degree()< C.degree())
+                {
+                    C_.increaseDegree();
+                } 
+            });
+    }
+    /**
+     * @brief unify the knots of a bspline curve set
+     * 
+     * @tparam Container 
+     * @param bs_lst 
+     */
+    template <typename Container>
+    auto unify_nodes(Container &bs_lst) -> void
+    {
+        auto p = bs_lst.front().degree();
+        std::for_each(bs_lst.begin(),bs_lst.end(),[&p](const auto &C_)
+        {
+            if (C_.degree() != p)
+                throw std::exception("unify_nodes: need curves with same degree");
+        });
+
+        auto &C = bs_lst.front();
+
+        std::for_each(
+            std::next(bs_lst.begin()),
+            bs_lst.end(),
+            [&](auto &C_) {
+                C_.changeBounds(C.bounds());
+                auto km_ = unflat_knots(C_.knotsFlats());
+                C.insertKnots( km_ );
+            });
+
+        auto km = unflat_knots(C.knotsFlats());
+
+        std::for_each(
+            std::next(bs_lst.begin()),
+            bs_lst.end(),
+            [&](auto &C_) {
+                C_.insertKnots( km );
+            });
+    }
 } // namespace occt_utils
--- tests/tests_bsctools.cpp
+++ tests/tests_bsctools.cpp
@@ -35,3 +35,55 @@ TEST(tests_bsctools, trim)
 
     occt_utils::to_iges(crv_lst, "trim.igs");
 }
+
+TEST(tests_bsctools, unify_nodes)
+{
+
+    std::vector<double> k1 = {0., 0., 0., 1, 2, 3, 4, 5., 5., 5.};
+    std::vector<double> k2 = {0., 0., 0.1, 1.3, 1.8 , 2.1, 2.6,  3., 3.};
+    std::vector<std::array<double,3> > poles =
+    {
+        {0.,0.,0.},
+        {0.,1.,0.},
+        {1.,1.,0.},
+        {1.,1.,1.},
+        {1.,1.,2.},
+        {3.,1.,1.},
+        {0.,4.,1.},
+    };
+    size_t p1 = 2;
+    size_t p2 = 1;
+    
+    gbs::BSCurve3d_d c1(poles,k1,p1);
+    gbs::BSCurve3d_d c2(poles,k2,p2);
+
+    std::list<gbs::BSCurve3d_d> bsc_lst = {c1,c2};
+
+    ASSERT_THROW( gbs::unify_nodes(bsc_lst) , std::exception);
+    
+    gbs::unify_degree(bsc_lst);
+
+    gbs::unify_nodes(bsc_lst);
+
+    auto k1_ = bsc_lst.front().knotsFlats();
+    auto k2_ = bsc_lst.back().knotsFlats();
+
+    ASSERT_TRUE(
+        std::equal(
+        k1_.begin(),
+        k1_.end(),
+        k2_.begin(),
+        [](const auto &k1,const auto &k2)
+        {
+            return fabs( k1 - k2 ) < gbs::knot_eps;
+        })
+    );
+
+    std::vector<Handle(Geom_Geometry)> crv_lst;
+    crv_lst.push_back(occt_utils::BSplineCurve(c1));
+    crv_lst.push_back(occt_utils::BSplineCurve(c2));
+    crv_lst.push_back(occt_utils::BSplineCurve(bsc_lst.front()));
+    crv_lst.push_back(occt_utils::BSplineCurve(bsc_lst.back()));
+
+    occt_utils::to_iges(crv_lst, "unify_nodes.igs");
+}
