9661a75e5a9ddc83ea92a0e3bbaf6ae8c53c0c99
szebos <sebastien.newe@gmail.com>
2020-11-01 10:38:05 +0100

    switch to pytest

--- python/tests/test_BSCinterp.py
+++ python/tests/test_BSCinterp.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 import sys
 
 # TODO: check if better using install path
@@ -7,32 +7,32 @@ import pygbs
 
 tol = 1e-6
 
-class TestBSCinterp(unittest.TestCase):
-    def distance(self,v1,v2):
-        return sum( (x-y)**2 for x , y in zip(v1,v2))
-    def test_cn(self):
 
-        pts = [
-            [0.,0.,0],
-            [0.,0.,1],
-            [1.,0.,0.5],
-            [1.,1.,1]
-        ]
-        constrains = []
-        for p in pts:
-            constrains.append([p])
+def distance(v1,v2):
+    return sum( (x-y)**2 for x , y in zip(v1,v2))
+def test_cn():
 
-        crv = pygbs.interpolate_cn_3d_d(
-            constrains,
-            2,
-            pygbs.KnotsCalcMode.CHORD_LENGTH
-        )
+    pts = [
+        [0.,0.,0],
+        [0.,0.,1],
+        [1.,0.,0.5],
+        [1.,1.,1]
+    ]
+    constrains = []
+    for p in pts:
+        constrains.append([p])
 
-        self.assertLess( self.distance(crv.begin(),pts[0]),tol)
-        self.assertLess( self.distance(crv.end(),pts[-1]),tol)
+    crv = pygbs.interpolate_cn_3d_d(
+        constrains,
+        2,
+        pygbs.KnotsCalcMode.CHORD_LENGTH
+    )
 
-        for p in pts:
-            result = pygbs.extrema_PC(crv,p,tol)
-            self.assertLess(result.d,tol)
-            self.assertLess(self.distance(crv.value(result.u),p),tol)
+    assert  distance(crv.begin(),pts[0]) <= tol
+    assert  distance(crv.end(),pts[-1]) <= tol
+
+    for p in pts:
+        result = pygbs.extrema_PC(crv,p,tol)
+        assert result.d <= tol
+        assert distance(crv.value(result.u),p) <= tol
     
--- python/tests/test_BSCurve.py
+++ python/tests/test_BSCurve.py
@@ -1,5 +1,5 @@
 import sys
-import unittest
+import pytest
 
 # TODO: check if better using install path
 sys.path.insert(1, 'build/')
@@ -7,95 +7,94 @@ import pygbs
 
 tol = 1e-6
 
-class TestBSCurve(unittest.TestCase):
-    def test_ctor(self):
+def test_ctor():
 
-        crv = pygbs.BSCurve3d_d(
-            [[0.,0.,0.],[1.,0.,0.]],
-            [0.,0.,1.,1.],
-            1)
+    crv = pygbs.BSCurve3d_d(
+        [[0.,0.,0.],[1.,0.,0.]],
+        [0.,0.,1.,1.],
+        1)
 
-        x = crv.value(0.5)
+    x = crv.value(0.5)
 
-        self.assertAlmostEqual(x[0],0.5,tol)
-        self.assertAlmostEqual(x[1],0.,tol)
-        self.assertAlmostEqual(x[2],0.,tol)
+    assert x[0] == pytest.approx(0.5,tol)
+    assert x[1] == pytest.approx(0.,tol)
+    assert x[2] == pytest.approx(0.,tol)
 
-        crv = pygbs.BSCurve2d_d(
-            [[0.,0.],[1.,0.]],
-            [0.,0.,1.,1.],
-            1)
+    crv = pygbs.BSCurve2d_d(
+        [[0.,0.],[1.,0.]],
+        [0.,0.,1.,1.],
+        1)
 
-        x = crv.value(0.5)
+    x = crv.value(0.5)
 
-        self.assertAlmostEqual(x[0],0.5,tol)
-        self.assertAlmostEqual(x[1],0.,tol)
+    assert x[0] == pytest.approx(0.5,tol)
+    assert x[1] == pytest.approx(0.,tol)
 
-        crv = pygbs.BSCurve1d_d(
-            [[0.],[1.]],
-            [0.,0.,1.,1.],
-            1)
+    crv = pygbs.BSCurve1d_d(
+        [[0.],[1.]],
+        [0.,0.,1.,1.],
+        1)
 
-        x = crv.value(0.5)
+    x = crv.value(0.5)
 
-        self.assertAlmostEqual(x[0],0.5,tol)
-        # simple precision
-        crv = pygbs.BSCurve3d_f(
-            [[0.,0.,0.],[1.,0.,0.]],
-            [0.,0.,1.,1.],
-            1)
+    assert x[0] == pytest.approx(0.5,tol)
+    # simple precision
+    crv = pygbs.BSCurve3d_f(
+        [[0.,0.,0.],[1.,0.,0.]],
+        [0.,0.,1.,1.],
+        1)
 
-        x = crv.value(0.5)
+    x = crv.value(0.5)
 
-        self.assertAlmostEqual(x[0],0.5,tol)
-        self.assertAlmostEqual(x[1],0.,tol)
-        self.assertAlmostEqual(x[2],0.,tol)
+    assert x[0] == pytest.approx(0.5,tol)
+    assert x[1] == pytest.approx(0.,tol)
+    assert x[2] == pytest.approx(0.,tol)
 
-        crv = pygbs.BSCurve2d_f(
-            [[0.,0.],[1.,0.]],
-            [0.,0.,1.,1.],
-            1)
+    crv = pygbs.BSCurve2d_f(
+        [[0.,0.],[1.,0.]],
+        [0.,0.,1.,1.],
+        1)
 
-        x = crv.value(0.5)
+    x = crv.value(0.5)
 
-        self.assertAlmostEqual(x[0],0.5,tol)
-        self.assertAlmostEqual(x[1],0.,tol)
+    assert x[0] == pytest.approx(0.5,tol)
+    assert x[1] == pytest.approx(0.,tol)
 
-        crv = pygbs.BSCurve1d_f(
-            [[0.],[1.]],
-            [0.,0.,1.,1.],
-            1)
+    crv = pygbs.BSCurve1d_f(
+        [[0.],[1.]],
+        [0.,0.,1.,1.],
+        1)
 
-        x = crv.value(0.5)
+    x = crv.value(0.5)
 
-        self.assertAlmostEqual(x[0],0.5,tol)
+    assert x[0] == pytest.approx(0.5,tol)
 
-        # Rational
-        crv = pygbs.BSCurveRational3d_d(
-            [[0.,0.,0.,1.],[1.,0.,0.,1.]],
-            [0.,0.,1.,1.],
-            1)
+    # Rational
+    crv = pygbs.BSCurveRational3d_d(
+        [[0.,0.,0.,1.],[1.,0.,0.,1.]],
+        [0.,0.,1.,1.],
+        1)
 
-        x = crv.value(0.5)
+    x = crv.value(0.5)
 
-        self.assertAlmostEqual(x[0],0.5,tol)
-        self.assertAlmostEqual(x[1],0.,tol)
-        self.assertAlmostEqual(x[2],0.,tol)
+    assert x[0] == pytest.approx(0.5,tol)
+    assert x[1] == pytest.approx(0.,tol)
+    assert x[2] == pytest.approx(0.,tol)
 
-        crv = pygbs.BSCurve2d_d(
-            [[0.,0.],[1.,0.]],
-            [0.,0.,1.,1.],
-            1)
+    crv = pygbs.BSCurve2d_d(
+        [[0.,0.],[1.,0.]],
+        [0.,0.,1.,1.],
+        1)
 
-        x = crv.value(0.5)
+    x = crv.value(0.5)
 
-        self.assertAlmostEqual(x[0],0.5,tol)
-        self.assertAlmostEqual(x[1],0.,tol)
+    assert x[0] == pytest.approx(0.5,tol)
+    assert x[1] == pytest.approx(0.,tol)
 
-        crv = pygbs.BSCurve1d_d(
-            [[0.],[1.]],
-            [0.,0.,1.,1.],
-            1)
+    crv = pygbs.BSCurve1d_d(
+        [[0.],[1.]],
+        [0.,0.,1.,1.],
+        1)
 
-        x = crv.value(0.5)
+    x = crv.value(0.5)
         
