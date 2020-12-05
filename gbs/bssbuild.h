f4060b9c54eb2bcb76bf1060d4769ae75d62116e
szebos <sebastien.newe@gmail.com>
2020-12-05 12:12:24 +0100

    add lost with spine support

--- gbs/bssbuild.h
+++ gbs/bssbuild.h
@@ -3,6 +3,8 @@
 #include <gbs/bscinterp.h>
 #include <gbs/bssurf.h>
 #include <gbs/bsctools.h>
+#include <boost/range/combine.hpp>
+// #include <boost/foreach.hpp>
 namespace gbs
 {
     ///
@@ -49,26 +51,9 @@ namespace gbs
         return bs_lst_cpy;
     }
 
-    // TODO try to make template container work
-    // template <typename T, size_t dim,bool rational, template<typename> typename Container >
-    // auto loft(const Container<BSCurveGeneral<T, dim,rational>*> &bs_lst,size_t v_degree_max = 3)
-    template <typename T, size_t dim,bool rational>
-    auto loft(const std::list<BSCurveGeneral<T, dim,rational>*> &bs_lst,size_t v_degree_max = 3)
+    template <typename T, size_t dim,bool rational,typename Container>
+    auto get_v_aligned_poles(size_t n_poles_u,size_t n_poles_v,const Container &bs_lst_cpy)
     {
-        if(bs_lst.size()<2)
-        {
-            throw std::length_error("loft needs at least 2 curves.");
-        }
-        auto bs_lst_cpy = get_BSCurves_cpy(bs_lst);
-        // static auto dim_poles = dim + rational; 
-
-        unify_degree(bs_lst_cpy);
-        unify_knots(bs_lst_cpy);
-
-        auto n_poles_v = bs_lst_cpy.size();
-        auto n_poles_u = bs_lst_cpy.front().poles().size();
-        auto ku_flat = bs_lst_cpy.front().knotsFlats();
-        auto p = bs_lst_cpy.front().degree();
         // get v aligned poles
         std::vector< points_vector<T,dim + rational> > poles_v_lst{ n_poles_u };
         std::vector< std::vector<T> > poles_v_params{ n_poles_u };
@@ -98,6 +83,32 @@ namespace gbs
             }
             v[j] /=  n_poles_u;
         }
+        return std::make_tuple(poles_v_params,poles_v_lst,v);
+    }
+
+    // TODO try to make template container work
+    // template <typename T, size_t dim,bool rational, template<typename> typename Container >
+    // auto loft(const Container<BSCurveGeneral<T, dim,rational>*> &bs_lst,size_t v_degree_max = 3)
+    template <typename T, size_t dim,bool rational>
+    auto loft(const std::list<BSCurveGeneral<T, dim,rational>*> &bs_lst,size_t v_degree_max = 3)
+    {
+        if(bs_lst.size()<2)
+        {
+            throw std::length_error("loft needs at least 2 curves.");
+        }
+        auto bs_lst_cpy = get_BSCurves_cpy(bs_lst);
+        // static auto dim_poles = dim + rational; 
+
+        unify_degree(bs_lst_cpy);
+        unify_knots(bs_lst_cpy);
+
+        auto n_poles_v = bs_lst_cpy.size();
+        auto n_poles_u = bs_lst_cpy.front().poles().size();
+        auto ku_flat = bs_lst_cpy.front().knotsFlats();
+        auto p = bs_lst_cpy.front().degree();
+        
+        auto [poles_v_params,poles_v_lst,v] = get_v_aligned_poles<T,dim,rational>(n_poles_u,n_poles_v,bs_lst_cpy);
+
         // interpolate poles
         size_t q = fmax(fmin(v_degree_max,n_poles_v-1),1); // degree V
         auto kv_flat = build_simple_mult_flat_knots<T>(v,n_poles_v,q);
@@ -111,6 +122,93 @@ namespace gbs
                 pts.end(),
                 Q.begin(),
                 [](const auto &p){constrType<T,dim + rational,1> c{p}; return c;}
+            );
+            auto poles_v = build_poles(Q, kv_flat, v, q);
+            poles.insert( poles.end(), poles_v.begin(),poles_v.end() );
+        }
+        // build surf
+        typedef std::conditional<rational,BSSurfaceRational<T, dim>,BSSurface<T, dim>>::type bs_type;
+        return bs_type( invert_uv_poles(poles,n_poles_u),  ku_flat, kv_flat, p , q );
+    }
+
+    template <typename T, size_t dim,bool rational>
+    auto loft(const std::list<BSCurveGeneral<T, dim,rational>*> &bs_lst,const BSCurve<T, dim> &spine,size_t v_degree_max = 3)
+    {
+        if(bs_lst.size()<2)
+        {
+            throw std::length_error("loft needs at least 2 curves.");
+        }
+        auto bs_lst_cpy = get_BSCurves_cpy(bs_lst);
+
+        // static auto dim_poles = dim + rational; 
+
+        unify_degree(bs_lst_cpy);
+        unify_knots(bs_lst_cpy);
+
+        auto n_poles_v = bs_lst_cpy.size(); 
+        auto n_poles_u = bs_lst_cpy.front().poles().size();
+        auto ku_flat = bs_lst_cpy.front().knotsFlats();
+        auto p = bs_lst_cpy.front().degree();
+        auto [poles_v_params, poles_v_lst, v] = get_v_aligned_poles<T, dim, rational>(n_poles_u, n_poles_v, bs_lst_cpy);
+        // // get v aligned poles
+        // std::vector< points_vector<T,dim + rational> > poles_v_lst{ n_poles_u };
+        // std::vector< std::vector<T> > poles_v_params{ n_poles_u };
+        // for(auto i = 0 ; i < n_poles_u; i++)
+        // {
+        //     points_vector<T,dim + rational> poles_v{ n_poles_v };
+        //     std::transform(
+        //         std::execution::par,
+        //         bs_lst_cpy.begin(),
+        //         bs_lst_cpy.end(),
+        //         poles_v.begin(),
+        //         [&i](const auto &crv)
+        //         {
+        //             return crv.poles()[i];
+        //         }
+        //     );
+        //     poles_v_params[i] = curve_parametrization(poles_v, KnotsCalcMode::CHORD_LENGTH, true);
+        //     std::swap( poles_v_lst[i] , poles_v );
+        // }
+        // // build parametrization
+        // std::vector<T> v(n_poles_v,0.);
+        // for(auto j = 0 ; j < n_poles_v; j++)
+        // {
+        //     for(auto i = 0 ; i < n_poles_u; i++)
+        //     {
+        //         v[j] += poles_v_params[i][j];
+        //     }
+        //     v[j] /=  n_poles_u;
+        // }
+        // compute positions on spine
+        std::vector<T> v_spine(bs_lst_cpy.size());
+        std::transform(
+                std::execution::par,
+                bs_lst_cpy.begin(),
+                bs_lst_cpy.end(),
+                v_spine.begin(),
+                [&spine](const auto &profile_)
+                {
+                    return extrema_CC(spine,profile_,1e-6).u1;
+                }
+        );
+        // interpolate poles
+        size_t q = fmax(fmin(v_degree_max,n_poles_v-1),1); // degree V // diff
+        auto kv_flat = build_simple_mult_flat_knots<T>(v,n_poles_v * 2,q); //dif
+        points_vector<T,dim + rational> poles;
+        std::vector<constrType<T,dim + rational,2> > Q(n_poles_v); //diff
+
+        for (auto pts : poles_v_lst)
+        {
+            auto pt_and_bs = boost::combine(pts,v_spine);
+            std::transform(
+                // std::execution::par, // not working with boost::combine
+                pt_and_bs.begin(),
+                pt_and_bs.end(),
+                Q.begin(),
+                [&](const auto &pt_and_bs_){
+                    auto [p,v_] = pt_and_bs_;
+                    return constrType<T,dim + rational,2> {p,spine.value(v_,1)};
+                    }
             );
             auto poles_v = build_poles(Q, kv_flat, v, q);
             poles.insert( poles.end(), poles_v.begin(),poles_v.end() );
--- tests/tests_bssbuild.cpp
+++ tests/tests_bssbuild.cpp
@@ -126,3 +126,49 @@ TEST(tests_bssbuild, loft_rational)
     auto s = gbs::loft( bs_lst );
     gbs::plot(s,c1,c2,c3);
 }
+
+TEST(tests_bssbuild, loft_with_spine)
+{
+    size_t p = 2;
+    std::vector<double> k = {0., 0., 0., 1, 2, 3, 4, 5., 5., 5.};
+    gbs::points_vector_3d_d poles1 =
+    {
+        {0.,0.,0.},
+        {0.,1.,0.},
+        {0.7,1.,0.},
+        {1.,1.3,0.},
+        {1.8,1.,0.3},
+        {3.,1.,0.},
+        {0.,4.,0.},
+    };
+    gbs::points_vector_3d_d poles2 =
+    {
+        {0.,0.,1.},
+        {0.,1.,1.},
+        {1.,1.,1.},
+        {1.3,0.4,1.},
+        {1.5,0.5,1.},
+        {3.,1.,1.5},
+        {2.,4.,1.},
+    };
+    gbs::points_vector_3d_d poles3 =
+    {
+        {0.,0.,2.},
+        {0.,1.,2.},
+        {0.5,1.,2.},
+        {1.,1.,2.5},
+        {1.5,1.,2.5},
+        {3.,1.,2.},
+        {1.,4.,2.},
+    };
+    gbs::BSCurve3d_d c1(poles1,k,p);
+    gbs::BSCurve3d_d c2(poles2,k,p);
+    gbs::BSCurve3d_d c3(poles3,k,p);
+    gbs::BSCurve3d_d sp({{1.5,1.5,0.},{1.5,1.5,3.}},{0.,0.,1.,1.},1);
+    auto d = sp.value(0.,1);
+
+    std::list<gbs::BSCurveGeneral<double,3,false>*> bs_lst = {&c1,&c2,&c3};
+    auto s = gbs::loft( bs_lst, sp );
+    gbs::plot(s,c1,c2,c3,sp);
+    // gbs::plot(s);
+}
