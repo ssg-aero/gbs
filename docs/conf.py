939310f11038a699d9367ec758bee7e9ec366ba0
szebos <sebastien.newe@gmail.com>
2020-12-18 08:21:47 +0100

    add read the doc script

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,13 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import subprocess, os
 
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+
+    subprocess.call('cd ../doxygen; doxygen', shell=True)
 
 # -- Project information -----------------------------------------------------
 
