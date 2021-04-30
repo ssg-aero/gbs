b6980f9f1380e77b4969396256fa6a68834da5ee
szebos <sebastien.newe@gmail.com>
2021-04-30 09:59:29 +0200

    add gradient to solvers

--- gbs/solvers.h
+++ gbs/solvers.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <nlopt.hpp>
 #include <vector>
+#include <numeric>
 namespace gbs
 {
     template <typename F>
@@ -10,16 +11,8 @@ namespace gbs
         const F &f_eq_;
     };
 
-    template <typename F1,typename F2>
-    struct G_UserData
-    {
-        G_UserData(const F1 &f_eq, const F2 &f_grad) : f_eq_{f_eq}, f_grad_{f_grad} {}
-        const F1 &f_eq_;
-        const F2 &f_grad_;
-    };
-
     template <typename F, typename T>
-    auto solve_N_nlop(const F &f, std::vector<T> &x, const std::vector<T> &lb, const std::vector<T> &hb,T tol_x, const char *solver)
+    auto solve_N_nlop(const F &f, std::vector<T> &x, const std::vector<T> &lb, const std::vector<T> &hb,T tol_x, nlopt::algorithm solver)
     {
 
         auto eq_ = [](const std::vector<double> &x, std::vector<double> &grad, void *user_data) {
@@ -32,6 +25,45 @@ namespace gbs
         };
 
         N_UserData data(f);
+
+        nlopt::opt opt(solver, x.size());
+
+        opt.set_lower_bounds(lb);
+        opt.set_upper_bounds(hb);
+        opt.set_min_objective(eq_, &data);
+        opt.set_xtol_rel(tol_x);
+
+        T minf;
+
+        opt.optimize(x, minf); 
+
+        return minf;
+    }
+
+    template <typename F1,typename F2>
+    struct D_UserData
+    {
+        D_UserData(const F1 &f_eq, const F2 &f_grad) : f_eq_{f_eq}, f_grad_{f_grad} {}
+        const F1 &f_eq_;
+        const F2 &f_grad_;
+    };
+
+    template <typename F1, typename F2, typename T>
+    auto solve_D_nlop(const F1 &f, const F2 &g, std::vector<T> &x, const std::vector<T> &lb, const std::vector<T> &hb,T tol_x, nlopt::algorithm solver)
+    {
+
+        auto eq_ = [](const std::vector<double> &x, std::vector<double> &grad, void *user_data) {
+            auto p_d = (D_UserData<decltype(f),decltype(g)> *)(user_data);
+            auto r = p_d->f_eq_(x);
+            if (!grad.empty())
+            {
+                grad = p_d->f_grad_(x,r); // passing f_eq_ evaluation can, I some occasions save some computation steps
+            }
+            std::transform(r.begin(),r.end(),r.begin(),[](const auto &r_){return r_*r_;});
+            return std::reduce(r.begin(),r.end());
+        };
+
+        D_UserData data(f,g);
 
         nlopt::opt opt(solver, x.size());
 
--- tests/tests_solvers.cpp
+++ tests/tests_solvers.cpp
@@ -3,43 +3,96 @@
 #include <gbs/gbslib.h>
 #include <gbs/vecop.h>
 #include <numbers>
+using std::cos;
+using std::sin;
+using std::exp;
+using std::numbers::sqrt2;
+using std::numbers::pi;
+using gbs::operator-;
+
 TEST(tests_solvers, solve_N_nlop)
 {
-    using std::cos;
-    using std::sin;
-    using std::numbers::sqrt2;
-    using std::numbers::pi;
-    using gbs::operator-;
 
-    auto r = 0.2;
-    gbs::point<double,2> Oc{0.,1.};
-    gbs::point<double,2> Od{0.,0.};
-    gbs::point<double,2> D{1.,0.};
+        auto r = 0.2;
+        gbs::point<double, 2> Oc{0., 1.};
+        gbs::point<double, 2> Od{0., 0.};
+        gbs::point<double, 2> D{1., 0.};
 
-    auto f = [=,&Oc,&Od,&D](const std::vector<double> &x)
-    {
-        auto th = x[0];
-        auto  u = x[1];
-        auto Pc = gbs::point<double,2>{r*cos(th)+Oc[0],r*sin(th)+Oc[1]};
-        auto Pd = gbs::point<double,2>{Od[0]+u*D[0],Od[1]+u*D[1]};
-        auto d = gbs::norm(Pc-Pd);
-        return d;
-    };
+        auto f = [=, &Oc, &Od, &D](const std::vector<double> &x) {
+            auto th = x[0];
+            auto u = x[1];
+            auto Pc = gbs::point<double, 2>{r * cos(th) + Oc[0], r * sin(th) + Oc[1]};
+            auto Pd = gbs::point<double, 2>{Od[0] + u * D[0], Od[1] + u * D[1]};
+            auto d = gbs::norm(Pc - Pd);
+            return d;
+        };
 
-    std::vector<double> x{0.,0.};
-    std::vector<double> lb{0.,-1.};
-    std::vector<double> hb{2*pi,1.};
-    auto tol = 1.e-8;
-    auto minf = gbs::solve_N_nlop(
-        f,
-        x,lb,hb,
-        tol,
-        // "LN_COBYLA"
-        "GN_ORIG_DIRECT"
-    );
+        std::vector<double> x{0., 0.};
+        std::vector<double> lb{0., -1.};
+        std::vector<double> hb{2 * pi, 1.};
+        auto tol = 1.e-8;
+        auto minf = gbs::solve_N_nlop(
+            f,
+            x, lb, hb,
+            tol,
+            // "LN_COBYLA"
+            // "GN_ORIG_DIRECT"
+            nlopt::GN_ORIG_DIRECT
+            );
 
-    ASSERT_NEAR(minf,0.8,2.*tol);
-    ASSERT_NEAR(x[0],3.*pi/2.,2.*tol);
-    ASSERT_NEAR(x[1],0.,2.*tol);
+        ASSERT_NEAR(minf, 0.8, 2. * tol);
+        ASSERT_NEAR(x[0], 3. * pi / 2., 2. * tol);
+        ASSERT_NEAR(x[1], 0., 2. * tol);
+
+    // solve with gradian
+    
 
 }
+
+TEST(tests_solvers, solve_D_nlop)
+    {
+        auto a = 2.;
+        auto b = 1.;
+        auto c = 3.;
+        auto d = 1.;
+        auto Y1= 2.;
+        auto Y2= 6.;
+        auto e = [](double x){return exp(x);};
+        auto f = [a,b,c,d,Y1,Y2,&e](const std::vector<double> &X)
+        {
+            auto x = X[0];
+            auto y = X[1];
+            auto r1=a*x*x+b*y-Y1;
+            auto r2=c*x+d*e(y)-Y2;
+
+            return std::vector<double>{r1,r2};
+        };
+        auto g = [a,b,c,d,&e](const std::vector<double> &X,const std::vector<double> &r) {
+            // std::vector<double> grad(X.size());
+            auto x = X[0];
+            auto y = X[1];
+            auto df0dx = 2.*a*x ;
+            auto df1dx = c ;
+            auto df0dy = b ;
+            auto df1dy = d * e(y) ;
+            // f outo put is euclidian norm
+            auto g1 = 2.* ( r[0] * df0dx + r[1] * df1dx );
+            auto g2 = 2.* ( r[0] * df0dy + r[1] * df1dy );
+            
+            return std::vector<double>{g1,g2};
+        };
+
+        std::vector<double> x{0., 0.};
+        std::vector<double> lb{-10., -10.};
+        std::vector<double> hb{10., 10.};
+        auto tol = 1.e-8;
+        auto minf = gbs::solve_D_nlop(
+            f,g,
+            x, lb, hb,
+            tol,
+            // nlopt::LN_COBYLA
+            // nlopt::LD_TNEWTON_PRECOND
+            nlopt::LD_CCSAQ
+            );
+        ASSERT_NEAR(minf,0.,tol);
+    }
