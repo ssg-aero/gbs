b600a9b7002a7bdcd1feec7e5c5ad84549c72008
szebos <sebastien.newe@gmail.com>
2021-05-14 11:59:14 +0200

    Improve discretization speed

--- gbs/bssanalysis.h
+++ gbs/bssanalysis.h
@@ -1,22 +1,36 @@
 #pragma once
 #include <gbs/bssurf.h>
-
 namespace gbs
 {
     template <typename T, size_t dim>
-    auto discretize(const Surface<T,dim> &srf, size_t nu, size_t nv) -> gbs::points_vector<T,dim>
+    auto discretize(const Surface<T, dim> &srf, size_t nu, size_t nv) -> gbs::points_vector<T, dim>
     {
-        points_vector<T,dim> points(nu*nv);
+        points_vector<T, dim> points;
         auto [u1, u2, v1, v2] = srf.bounds();
-        auto du = (u2-u1) / (nu-1);
-        auto dv = (v2-v1) / (nv-1);
+        auto u = make_range(u1, u2, nu);
+        auto v = make_range(v1, v2, nv);
 
-        auto v = v1;
-        for(auto it = points.begin() ; it!= points.end() ; it=std::next(it,nu) )
-        {
-            std::generate(it,std::next(it,nu),[&,u=u1-du]() mutable {return srf.value(u+=du,v);});
-            v+=dv;
-        }
+        std::for_each(
+            v.begin(),
+            v.end(),
+            [&srf, &u, &points, nu](const auto &v_) // mutable
+            {
+                points_vector<T, dim> points_(nu);
+                std::transform(
+                    std::execution::par,
+                    u.begin(),
+                    u.end(),
+                    points_.begin(),
+                    [&srf, v_](const auto &u_)
+                    {
+                        return srf(u_, v_);
+                    });
+
+                points.insert(
+                    points.end(),
+                    std::make_move_iterator(points_.begin()),
+                    std::make_move_iterator(points_.end()));
+            });
 
         return points;
     }
