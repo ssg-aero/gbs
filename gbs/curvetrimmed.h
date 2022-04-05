8c11783b00709fc478e5de364501c6f4f52e651f
szebos <sebastien.newe@gmail.com>
2022-04-05 15:29:31 +0200

    fix exception using tol

--- gbs/curvetrimmed.h
+++ gbs/curvetrimmed.h
@@ -13,7 +13,10 @@ namespace gbs
         CurveTrimmed(const std::shared_ptr<Curve<T, dim>> &p_crv, T u1, T u2) : p_crv_{p_crv}, bounds_{u1,u2} {}
         virtual auto value(T u, size_t d = 0) const -> std::array<T, dim> override
         {
-            assert(u>=bounds_[0] && u<=bounds_[1]);
+            if (u < this->bounds()[0] - knot_eps|| u > this->bounds()[1] + knot_eps)
+            {
+                throw OutOfBoundsCurveEval(u,this->bounds());
+            }
             return p_crv_->value(u,d);
         }
         virtual auto bounds() const -> std::array<T, 2>
