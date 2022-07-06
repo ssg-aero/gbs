457b9fdd306c24e6905b8121f1288e650891f0cc
Adrien DELSALLE <ad.delsalle@gmail.com>
2022-07-06 10:56:04 +0200

    update conda recipe build scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,15 +3,17 @@ cd build-conda
 
 cmake .. \
 -D CMAKE_BUILD_TYPE:STRING="Release" \
--D USE_OCCT_UTILS:BOOL=FALSE \
--D GBS_BUILD_TESTS:BOOL=FALSE \
--D USE_RENDER:BOOL=TRUE \
--D USE_PYTHON_BINDINGS=TRUE \
--D BUILD_DOC=FALSE \
+-D BUILD_GBS=ON \
+-D BUILD_GBS_MESH=ON \
+-D BUILD_GBS_RENDER=ON \
+-D BUILD_PYTHON_BINDINGS=ON \
 -D CMAKE_INSTALL_PREFIX=$PREFIX \
 -G "Ninja" \
 -Wno-dev
 
 ninja install
+
+cd ../python
+$PYTHON -m pip install . --no-deps -vv
 
 cd ..
