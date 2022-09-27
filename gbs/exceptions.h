55987e19a923ce175b86dda1684cf45cbd9a8a6c
szebos <sebastien.newe@gmail.com>
2022-09-27 12:53:32 +0200

    fix incorrect message due to bad init order

--- gbs/exceptions.h
+++ gbs/exceptions.h
@@ -3,22 +3,12 @@
 #include <array>
 namespace gbs{
     template<typename T>
-    class OutOfBoundsEval : public std::out_of_range
+    class OutOfBoundsEval : public std::domain_error
     {
-        T m_val;
-        std::array<T,2> m_bounds;
-
     public:
         explicit OutOfBoundsEval(T v, const std::array<T, 2> &bounds, const char *eval_msg ="Eval ") : 
-            m_val{v}, 
-            m_bounds{bounds}, 
-            std::out_of_range{ eval_msg + msg()}
-        { }
-        auto val() const noexcept {return m_val;}
-        const auto& bounds() const noexcept {return m_bounds;}
-        auto msg()
-        {
-            return std::to_string(val()) + " out of bounds [ " +  std::to_string(bounds()[0]) + " , "  + std::to_string(bounds()[1]) + " ] error.";
-        }
+            std::domain_error{ "Eval "+  std::to_string(v) + " out of bounds [ " +  std::to_string(bounds[0]) + " , "  + std::to_string(bounds[1]) + " ] error."}
+            { }
+
     };
 }
