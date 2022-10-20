692d25c23b4bfea05801fdee20b54a45b17e6b36
szebos <sebastien.newe@gmail.com>
2022-10-20 08:48:19 +0200

    add ctor with mult

--- gbs/bssurf.h
+++ gbs/bssurf.h
@@ -564,7 +564,20 @@ namespace gbs
     public:
         BSSfunction(const BSSfunction<T> &f) = default;
         BSSfunction(const BSSurface<T, 1> &crv) : srf_{crv} {}
-        BSSfunction(const std::vector<T> &poles, const std::vector<T> &knots_flatsU, const std::vector<T> &knots_flatsV, size_t degU, size_t degV) : srf_{BSCfunction<T>::poles_array(poles), knots_flatsU, knots_flatsV, degU, degV} {}
+        BSSfunction(
+            const std::vector<T> &poles, 
+            const std::vector<T> &knots_flatsU, 
+            const std::vector<T> &knots_flatsV, 
+            size_t degU, 
+            size_t degV ) : srf_{BSCfunction<T>::poles_array(poles), knots_flatsU, knots_flatsV, degU, degV} {}
+        BSSfunction(
+            const std::vector<T> &poles, 
+            const std::vector<T> &knotsU, 
+            const std::vector<T> &knotsV,
+            const std::vector<size_t> &multsU,
+            const std::vector<size_t> &multsV,
+            size_t degU, 
+            size_t degV ) : srf_{BSCfunction<T>::poles_array(poles), knotsU, knotsV, multsU, multsV, degU, degV} {}
         auto operator()(T u, T v, size_t du = 0, size_t dv = 0) const -> T
         {
             return srf_.value(u, v, du, dv)[0];
--- python/gbsbind.cpp
+++ python/gbsbind.cpp
@@ -268,6 +268,7 @@ PYBIND11_MODULE(gbs, m) {
                 .def(py::init<const gbs::BSSfunction<double> &>())
                 .def(py::init<const gbs::BSSurface<double, 1> &>())
                 .def(py::init<const std::vector<double> &, const std::vector<double> &, const std::vector<double> &, size_t, size_t>())
+                .def(py::init<const std::vector<double> &, const std::vector<double> &, const std::vector<double> &, const std::vector<size_t> &, const std::vector<size_t> &, size_t, size_t>())
                 .def("basisSurface",&gbs::BSSfunction<double>::basisSurface)
                 .def("bounds",&gbs::BSSfunction<double>::bounds )
                 .def("__call__",&gbs::BSSfunction<double>::operator(),"Function evaluation at given parameter",py::arg("u"),py::arg("v"),py::arg("du") = 0,py::arg("dv") = 0)
