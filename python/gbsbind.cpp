a2bf4baf5a67279c10aaeffd006b3f4f1dce8f41
szebos <sebastien.newe@gmail.com>
2022-11-11 16:54:31 +0100

    move bsfunction binding

--- python/gbsbind.cpp
+++ python/gbsbind.cpp
@@ -236,48 +236,6 @@ PYBIND11_MODULE(gbs, m) {
         gbs_bind_surfaces(m);
 
 
-        py::class_<gbs::BSCfunction<double>>(m,"BSCfunction")
-                .def(py::init<const gbs::BSCfunction<double> &>())
-                .def(py::init<const BSCurve<double,1> &>())
-                .def(py::init<const std::vector<std::array<double, 1>> &, const std::vector<double> &, size_t >(),
-                        py::arg("poles"),py::arg("knots_flats"),py::arg("deg"))
-                .def(py::init<const std::vector<double> &, const std::vector<double> &, size_t >(),
-                        py::arg("poles"),py::arg("knots_flats"),py::arg("deg"))
-                .def(py::init<const std::vector<double> &, const std::vector<double> &, const std::vector<size_t> &, size_t >(),
-                        py::arg("poles"), py::arg("knots"), py::arg("mults"), py::arg("deg"))
-                .def("value",&gbs::BSCfunction<double>::value,"Function evaluation at given parameter",py::arg("u"),py::arg("d") = 0)
-                .def("basisCurve",&gbs::BSCfunction<double>::basisCurve )
-                .def("bounds",&gbs::BSCfunction<double>::bounds )
-                .def("__call__",&gbs::BSCfunction<double>::operator(),"Function evaluation at given parameter",py::arg("u"),py::arg("d") = 0)
-                // .def("__reduce__", [](gbs::BSCfunction<double> const &self) { // for pickle https://github.com/pybind/pybind11/issues/1261
-                //         return py::make_tuple(py::cpp_function([](){return gbs::BSCfunction<double>();}), py::make_tuple());
-                // })
-                // .def(py::pickle(
-                //         [](const gbs::BSCfunction<double> &f) {return py::make_tuple(f.basisCurve());},
-                //         [](py::tuple t){
-                //                 if (t.size() != 1)
-                //                         throw std::runtime_error("Invalid state!");
-                //                 gbs::BSCfunction<double> f{ t[0].cast<gbs::BSCurve<double,1>>};
-                //                 return f;
-                //         }
-                // ))
-                .def("__copy__",  [](const  gbs::BSCfunction<double> &self) {
-                        return  gbs::BSCfunction<double>(self);
-                })
-                ;
-        py::class_<gbs::BSSfunction<double>>(m,"BSSfunction")
-                .def(py::init<const gbs::BSSfunction<double> &>())
-                .def(py::init<const gbs::BSSurface<double, 1> &>())
-                .def(py::init<const std::vector<double> &, const std::vector<double> &, const std::vector<double> &, size_t, size_t>())
-                .def(py::init<const std::vector<double> &, const std::vector<double> &, const std::vector<double> &, const std::vector<size_t> &, const std::vector<size_t> &, size_t, size_t>())
-                .def("basisSurface",&gbs::BSSfunction<double>::basisSurface)
-                .def("bounds",&gbs::BSSfunction<double>::bounds )
-                .def("__call__",&gbs::BSSfunction<double>::operator(),"Function evaluation at given parameter",py::arg("u"),py::arg("v"),py::arg("du") = 0,py::arg("dv") = 0)
-                .def("__copy__",  [](const  gbs::BSSfunction<double> &self) {
-                        return  gbs::BSSfunction<double>(self);
-                })
-                ;
-
         m.def("eval",
                 py::overload_cast<
                         double,
