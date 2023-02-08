6b3efcc70bb40cba8de25f753e23482dd4368695
szebos <sebastien.newe@gmail.com>
2023-02-08 13:12:40 +0100

    add multi-constraints shaping

--- gbs/bscshaping.h
+++ gbs/bscshaping.h
@@ -1,4 +1,5 @@
 #include <gbs/curves>
+#include <gbs/bscapprox.h>
 
 namespace gbs
 {
@@ -13,6 +14,7 @@ namespace gbs
         }
         auto TB = B.transpose();
         auto Inv = (TB * B).llt();
+        std::array<Eigen::VectorXd, dim> DX;
         Eigen::VectorXd Dx(1), Dy(1), Dz(1);
         Dx(0) = D[0];
         Dy(0) = D[1];
@@ -42,7 +44,104 @@ namespace gbs
 
         move_to_point(poles, D, k, p , u, i1, i2);
 
-        return BSCurve<T,3>{poles,k,p};
+        return BSCurve<T,dim>{poles,k,p};
+
+    }
+
+    template<typename T, size_t dim>
+    auto move_to_point(BSCurve<T,dim> &crv, const point<T,dim> &pt, T u)
+    {       
+        auto poles = crv.poles();
+        auto n = poles.size();
+        auto k     = crv.knotsFlats();
+        auto p     = crv.degree();
+
+        auto [i1, i2] = find_span_range(n, p , u , k);
+        auto D = pt - crv(u);
+
+        move_to_point(poles, D, k, p , u, i1, i2);
+
+        crv.setPoles(poles);
+
+    }
+
+    template<typename T, size_t dim>
+    auto move_to_constraints_delta(points_vector<T, dim> &poles,const std::vector<bsc_constrain<T,dim>> &constraints_delta, const std::vector<T> &k, size_t p, size_t i1, size_t i2)
+    {
+        auto m = constraints_delta.size();
+        auto n = i2 - i1 + 1;
+        assert(m <= n );
+        Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> B(n, m);
+        for(size_t j{}; j< m; j++)
+        {
+            auto u = std::get<0>(constraints_delta[j]);
+            auto d = std::get<2>(constraints_delta[j]);
+            for (size_t i{i1}; i <= i2; i++)
+            {
+                B(i - i1, j) = basis_function(u, i, p, d, k);
+            }
+        }
+
+        std::array<Eigen::VectorXd, dim> DX;
+        std::array<Eigen::VectorXd, dim> DP;
+        for(size_t i{}; i < dim; i++){
+            DX[i] = Eigen::VectorXd(m);
+            for(size_t j{}; j< m; j++)
+            {
+                auto u = std::get<0>(constraints_delta[j]);
+                auto d = std::get<2>(constraints_delta[j]);
+                DX[i](j) = std::get<1>(constraints_delta[j])[i];
+            }
+        }
+        
+        if(m < n)
+        {
+            auto TB = B.transpose();
+            auto Inv = (TB * B).llt();
+            for(size_t i{}; i < dim; i++)
+            {
+                DP[i] =  B * Inv.solve(DX[i]) ;
+            }
+        }
+        else{
+            auto Inv = B.partialPivLu();
+            for(size_t i{}; i < dim; i++)
+            {
+                DP[i] =  Inv.solve(DX[i]) ;
+            }
+        }
+
+        for (size_t i{i1}; i <= i2; i++)
+        {
+            for(size_t j{}; j < dim; j++)
+            {
+                poles[i][j] = poles[i][j] + DP[j](i-i1);
+            }
+        }
+    }
+    template<typename T, size_t dim>
+    auto moved_to_constraints(const BSCurve<T,dim> &crv, const std::vector<bsc_constrain<T,dim>> &constraints)
+    {       
+        auto poles = crv.poles();
+        auto n = poles.size();
+        auto k     = crv.knotsFlats();
+        auto p     = crv.degree();
+
+        size_t i1{}, i2{n-1};
+        std::vector<bsc_constrain<T,dim>> constraints_delta;
+        for(auto &cstr : constraints)
+        {
+            auto u_ = std::get<0>(cstr);
+            auto d_ = std::get<2>(cstr);
+            auto [i1_, i2_] = find_span_range(n, p , u_ , k);
+            i1 = std::min(i1,i1_);
+            i2 = std::max(i2,i2_);
+            constraints_delta.push_back( {u_,  std::get<1>(cstr) - crv(u_, d_), d_} );
+        }
+        
+        move_to_constraints_delta(poles, constraints_delta, k, p , i1, i2);
+
+        return BSCurve<T,dim>{poles,k,p};
 
     }
 
--- tests/tests_curves_shapping.cpp
+++ tests/tests_curves_shapping.cpp
@@ -6,6 +6,7 @@
 using namespace gbs;
 using T = double;
 const bool PLOT_ON = false;
+
 TEST(curve_shaping, move_to_point)
 {
     std::vector<T> k = {0., 0., 0., 1, 2, 3, 4, 5., 5., 5.};
@@ -22,13 +23,70 @@ TEST(curve_shaping, move_to_point)
     size_t p = 2;
     T u = 2.3;
 
-
     auto crv = BSCurve<T,3>(poles,k,p);
     point<T,3> deltap{0.1,0.2,0.3}; 
     std::vector<std::array<T,3> > points{crv(u),crv(u)+deltap};
 
     auto new_crv = moved_to_point(crv, points[1], u);
     ASSERT_NEAR( distance(points[1], new_crv(u)), 0., 1e-9 );
+
+    if(PLOT_ON)
+    {
+        auto seg = build_segment(points[0], points[1]);
+        plot(
+            gbs::crv_dsp<T, 3, false>{
+                .c = &crv,
+                .col_crv = {1., 0., 0.},
+                .poles_on = true,
+                .col_poles = {0., 1., 0.},
+                .col_ctrl = {0., 0., 0.},
+                .show_curvature = false,
+            },
+            gbs::crv_dsp<T, 3, false>{
+                .c = &new_crv,
+                .col_crv = {0., 0., 1.},
+                .poles_on = true,
+                .col_poles = {0., 1., 0.},
+                .col_ctrl = {0., 0., 0.},
+                .show_curvature = false,
+            },
+
+            gbs::crv_dsp<T, 3, false>{
+                .c = &seg,
+                .col_crv = {0., 1., 0.},
+            },
+            points);
+    }
+}
+
+TEST(curve_shaping, to_constraints)
+{
+    const size_t dim{3};
+    std::vector<T> k = {0., 0., 0., 1, 2, 3, 4, 5., 5., 5.};
+    std::vector<std::array<T,dim> > poles =
+    {
+        {0.,0.,0.},
+        {0.,1.,0.},
+        {1.,1.,0.},
+        {1.,1.,1.},
+        {1.,1.,2.},
+        {3.,1.,1.},
+        {0.,4.,1.},
+    };
+    size_t p = 2;
+    T u = 2.3;
+    auto crv = BSCurve<T,3>(poles,k,p);
+    point<T,3> deltap{0.1,0.2,0.3}; 
+    std::vector<std::array<T,dim> > points{crv(u),crv(u)+deltap};
+
+    std::vector<bsc_constrain<T,dim>> constraints{
+        {u,points[1],0},
+        {u,crv(u,1),1},
+    };
+
+    auto new_crv = moved_to_constraints(crv, constraints);
+    for(auto &cstr : constraints)
+        ASSERT_NEAR( distance(std::get<1>(cstr), new_crv(std::get<0>(cstr),std::get<2>(cstr))), 0., 1e-9 );
 
     if(PLOT_ON)
     {
