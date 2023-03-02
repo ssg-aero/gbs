5d74d9461eb56b32f53500cf158dd3447f61e70b
szebos <sebastien.newe@gmail.com>
2023-03-02 12:59:38 +0100

    inefficient delauney mesh

--- inc/topology/tessellations.h
+++ inc/topology/tessellations.h
@@ -1227,4 +1227,98 @@ namespace gbs
 
         return faces_lst;
     }
+
+    template <typename T>
+    bool is_face_edges_inter(const gbs::HalfEdge<T,2> &h_e,const gbs::HalfEdgeFace<T, 2> &h_f )
+    {
+        assert(h_e.previous);
+        for(const auto &h_e_ : gbs::getFaceEdges(h_f) )
+        {
+            assert(h_e_->previous);
+            if(seg_seg_strict_intersection(
+                h_e.previous->vertex->coords,
+                h_e.vertex->coords,
+                h_e_->previous->vertex->coords,
+                h_e_->vertex->coords
+            ))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    template < typename T , auto _ExPo = std::execution::seq>
+    auto base_delaunay2d_mesh(std::vector< std::shared_ptr< HalfEdgeVertex<T,2> > > &vertices_cloud)
+    {
+        auto n = vertices_cloud.size();
+
+        std::list< std::shared_ptr< HalfEdgeFace<T,2> > > faces_lst;
+
+        for(size_t i{}; i < n; i++)
+        {
+            auto a = vertices_cloud[i];
+            // for(size_t j{i+1}; j < n; j++)
+            for(size_t j{}; j < n; j++)
+            {
+                auto b = vertices_cloud[j];
+                for( size_t k{j+1}; k < n ; k++)
+                // for( size_t k{}; k < n ; k++)
+                {
+                    auto c   = vertices_cloud[k];
+
+                    if(orient_2d(a->coords,b->coords,c->coords) > 0.) // avoids also null triangle
+                    {
+                    
+                        auto it_inside_vtx =  std::find_if(
+                            _ExPo,
+                            vertices_cloud.begin(),
+                            vertices_cloud.end(),
+                            [a_coords=a->coords,b_coords=b->coords,c_coords=c->coords](const auto &d) { return in_circle(a_coords, b_coords, c_coords, d->coords) > 0.; }
+                        );
+                        auto is_delauney_triangle {vertices_cloud.end() == it_inside_vtx};
+
+                        if(is_delauney_triangle)
+                        {
+                            // create potentially new face loop
+                            auto he1 = make_shared_h_edge(a);
+                            auto he2 = make_shared_h_edge(b);
+                            auto he3 = make_shared_h_edge(c);
+                            he1->previous = he3;
+                            he2->previous = he1;
+                            he3->previous = he2;
+
+                            bool cross_tri{false}, is_perm{false};
+                            for(const auto &h_face : faces_lst)
+                            {
+                                auto lst_vtx = {a, b, c};
+                                // check if loop is a permutation of previously defined face
+                                is_perm = std::is_permutation(lst_vtx.begin(), lst_vtx.end(), gbs::getFaceVertices(h_face).begin());
+                                if(is_perm)
+                                {
+                                    break;
+                                }
+                                // check if edges are intersection face
+                                cross_tri = 
+                                    is_face_edges_inter(*he1 , *h_face) ||
+                                    is_face_edges_inter(*he2 , *h_face) ||
+                                    is_face_edges_inter(*he3 , *h_face);
+                                if(cross_tri)
+                                {
+                                    break;
+                                }
+                            }
+                            if(!cross_tri && !is_perm)
+                            {
+                                auto lst1 = {he1, he2, he3};
+                                faces_lst.push_back( make_shared_h_face<T,2>(lst1) );
+                            }
+                        }
+                    
+                    }
+                }
+            }
+        }
+        return faces_lst;
+    }
 }
