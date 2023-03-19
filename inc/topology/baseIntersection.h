26e941a9084e85204b2e890c294317da7b737f41
Sébastien Raymond <sebastien.newe@gmail.com>
2023-03-19 19:10:15 +0100

    add uv coordinates computation

--- inc/topology/baseIntersection.h
+++ inc/topology/baseIntersection.h
@@ -3,55 +3,134 @@
 #include <gbs/vecop.h>
 namespace gbs
 {
-
-    template<typename T>
-    bool in_triangle(
-        const std::array<T, 2> &a,
-        const std::array<T, 2> &b,
-        const std::array<T, 2> &c,
-        const std::array<T, 2> &d)
+/**
+ * @brief Determines whether a given point D lies inside a triangle defined by points A, B, and C.
+ *
+ * @tparam T A floating-point type.
+ * @param a The coordinates of the triangle's first vertex, A.
+ * @param b The coordinates of the triangle's second vertex, B.
+ * @param c The coordinates of the triangle's third vertex, C.
+ * @param d The coordinates of the point D to be tested.
+ * @return `true` if the point D lies inside the triangle, `false` otherwise.
+ */
+    template<std::floating_point T>
+    [[nodiscard]] bool in_triangle(
+        const std::array<T, 2>& a,
+        const std::array<T, 2>& b,
+        const std::array<T, 2>& c,
+        const std::array<T, 2>& d)
     {
-        auto sign = [](const auto &p1, const auto &p2, const auto &p3){
-            return (p1[0]-p3[0])*(p2[1]-p3[1]) - (p2[0]-p3[0])*(p1[1]-p3[1]);
+        // Lambda function to compute the signed area of a triangle defined by three points
+        auto sign = [](const auto& p1, const auto& p2, const auto& p3) {
+            return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1]);
         };
 
+        // Calculate the signed areas of the triangles formed by point D and each edge of the input triangle
         auto d1 = sign(d, a, b);
         auto d2 = sign(d, b, c);
         auto d3 = sign(d, c, a);
 
+        // Determine if the point D has a consistent orientation with respect to the input triangle's vertices
         auto has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
         auto has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
 
         return !(has_neg && has_pos);
     }
-    /**
-     * @brief returns > 0 if d inside circle formed by (a, b, c) return 0. if on
-     * 
-     * @tparam T 
-     * @param ax 
-     * @param ay 
-     * @param bx 
-     * @param by 
-     * @param cx 
-     * @param cy 
-     * @param dx 
-     * @param dy 
-     * @return T 
-     */
-    template <typename T>
-    T in_circle( T ax, T ay, T bx, T by, T cx, T cy, T dx, T dy)
+/**
+ * @brief Computes the barycentric coordinates of a point in a triangle in 2D of non degenerated triangle.
+ * 
+ * @tparam T The type of the coordinates (must be a floating-point type).
+ * @param a The first vertex of the triangle.
+ * @param b The second vertex of the triangle.
+ * @param c The third vertex of the triangle.
+ * @param d The point for which to compute the barycentric coordinates.
+ * @return A pair of barycentric coordinates (u, v) such that d = u*a + v*b + (1-u-v)*c,
+ *         or false if the denominator of the calculation is zero or very close to zero.
+ */
+    template<std::floating_point T>
+    [[nodiscard]] auto triangle_barycentric_coordinates(
+            const std::array<T, 2>& a,
+            const std::array<T, 2>& b,
+            const std::array<T, 2>& c,
+            const std::array<T, 2>& d)
     {
-        auto A = ax - dx;
-        auto B = ay - dy;
-        auto C = A * A + B * B;
+        // Compute vectors
+        std::array<T, 2> v0{c[0] - a[0], c[1] - a[1]};
+        std::array<T, 2> v1{b[0] - a[0], b[1] - a[1]};
+        std::array<T, 2> v2{d[0] - a[0], d[1] - a[1]};
 
-        auto D = bx - dx;
-        auto E = by - dy;
-        auto F = D * D + E * E;
+        // Compute dot products
+        T dot00{v0[0] * v0[0] + v0[1] * v0[1]};
+        T dot01{v0[0] * v1[0] + v0[1] * v1[1]};
+        T dot02{v0[0] * v2[0] + v0[1] * v2[1]};
+        T dot11{v1[0] * v1[0] + v1[1] * v1[1]};
+        T dot12{v1[0] * v2[0] + v1[1] * v2[1]};
 
-        auto G = cx - dx;
-        auto H = cy - dy;
-        auto I = G * G + H * H;
+        // Compute the denominator
+        T denom{dot00 * dot11 - dot01 * dot01};
+
+        // Compute barycentric coordinates
+        T invDenom{ 1.0 / denom };
+        T u{(dot11 * dot02 - dot01 * dot12) * invDenom};
+        T v{(dot00 * dot12 - dot01 * dot02) * invDenom};
+        return std::make_pair(u, v);
+
+    }
+/**
+ * @brief Determines whether a given point D lies inside a triangle defined by points A, B, and C using barycentric coordinates.
+ *
+ * @tparam T A floating-point type.
+ * @param a The coordinates of the triangle's first vertex, A.
+ * @param b The coordinates of the triangle's second vertex, B.
+ * @param c The coordinates of the triangle's third vertex, C.
+ * @param d The coordinates of the point D to be tested.
+ * @return `true` if the point D lies inside the triangle, `false` otherwise.
+ */
+    template<std::floating_point T>
+    [[nodiscard]] bool in_triangle_barycentric(
+        const std::array<T, 2>& a,
+        const std::array<T, 2>& b,
+        const std::array<T, 2>& c,
+        const std::array<T, 2>& d)
+    {
+        // Check if d is equal to one of the triangle's vertices
+        if (d == a || d == b || d == c)
+        {
+            return true;
+        }
+
+        auto [u,v] = triangle_barycentric_coordinates(a, b, c, d);
+
+        return (u >= static_cast<T>(0)) && (v >= static_cast<T>(0)) && (u + v <= static_cast<T>(1));
+    }
+/**
+ * @brief Determines the position of the point D (dx, dy) with respect to the circle passing through points A (ax, ay), B (bx, by), and C (cx, cy).
+ * 
+ * @tparam T A floating-point type.
+ * @param ax The x-coordinate of point A.
+ * @param ay The y-coordinate of point A.
+ * @param bx The x-coordinate of point B.
+ * @param by The y-coordinate of point B.
+ * @param cx The x-coordinate of point C.
+ * @param cy The y-coordinate of point C.
+ * @param dx The x-coordinate of point D.
+ * @param dy The y-coordinate of point D.
+ * @return A positive value if the point D is inside the circle, a negative value if the point D is outside the circle, and zero if the point D is on the circle.
+ */
+    template <std::floating_point T>
+    [[nodiscard]] T in_circle(T ax, T ay, T bx, T by, T cx, T cy, T dx, T dy)
+    {
+        T A{ax - dx};
+        T B{ay - dy};
+        T C{A * A + B * B};
+
+        T D{bx - dx};
+        T E{by - dy};
+        T F{D * D + E * E};
+
+        T G{cx - dx};
+        T H{cy - dy};
+        T I{G * G + H * H};
 
         return A * E * I + D * H * C + B * F * G - (G * E * C + D * B * I + A * H * F);
     }
@@ -66,8 +145,8 @@ namespace gbs
      * @param d 
      * @return T 
      */
-    template <typename T>
-    T in_circle(
+    template <std::floating_point T>
+    [[nodiscard]] T in_circle(
         const std::array<T, 2> &a,
         const std::array<T, 2> &b,
         const std::array<T, 2> &c,
@@ -81,55 +160,47 @@ namespace gbs
         return in_circle( ax, ay, bx, by, cx, cy, dx, dy);
     }
 
-    /**
-     * @brief Intersection of segments ]a,b[ and ]c,d[
-     * 
-     * @tparam T 
-     * @param a 
-     * @param b 
-     * @param c 
-     * @param d 
-     * @param tol 
-     * @return true 
-     * @return false 
-     */
-    template <typename T>
-    bool seg_seg_strict_intersection(
-        const std::array<T,2> &a,
-        const std::array<T,2> &b,
-        const std::array<T,2> &c,
-        const std::array<T,2> &d,
-        T tol = 1e-10
+/**
+ * @brief Checks if two line segments strictly intersect (excluding endpoints).
+ * @tparam T Numeric type (e.g. float, double)
+ * @param a First point of the first line segment
+ * @param b Second point of the first line segment
+ * @param c First point of the second line segment
+ * @param d Second point of the second line segment
+ * @param tol Tolerance for floating-point comparisons (default: 1e-10)
+ * @return true if the line segments strictly intersect, false otherwise
+ */
+    template <std::floating_point T>
+    [[nodiscard]] bool seg_seg_strict_intersection(
+        const std::array<T, 2> &a,
+        const std::array<T, 2> &b,
+        const std::array<T, 2> &c,
+        const std::array<T, 2> &d,
+        T tol = T{1e-10}
     )
     {
-        T s1_x, s1_y, s2_x, s2_y;
-        s1_x = b[0] - a[0];
-        s1_y = b[1] - a[1];
-        s2_x = d[0] - c[0];
-        s2_y = d[1] - c[1];
+        T s1_x = b[0] - a[0];
+        T s1_y = b[1] - a[1];
+        T s2_x = d[0] - c[0];
+        T s2_y = d[1] - c[1];
 
-        T s, t, delta{-s2_x * s1_y + s1_x * s2_y};
+        T delta = -s2_x * s1_y + s1_x * s2_y;
 
-        const T max = 1-tol;
-        if(
-            std::fabs(delta)<tol 
-            || ( std::fabs(s1_x)<tol && std::fabs(s1_y)<tol) 
-            || ( std::fabs(s2_x)<tol && std::fabs(s2_y)<tol))
+        // Check if the segments are parallel or if they are degenerate (zero length)
+        if (std::abs(delta) < tol || (std::abs(s1_x) < tol && std::abs(s1_y) < tol) || (std::abs(s2_x) < tol && std::abs(s2_y) < tol))
         {
             return false;
         }
-        s = (-s1_y * (a[0] - c[0]) + s1_x * (a[1] - c[1])) / delta;
-        t = ( s2_x * (a[1] - c[1]) - s2_y * (a[0] - c[0])) / delta;
 
-        if (s > tol && s < max && t > tol && t < max)
-        {
-            return true;
-        }
+        T s = (-s1_y * (a[0] - c[0]) + s1_x * (a[1] - c[1])) / delta;
+        T t = (s2_x * (a[1] - c[1]) - s2_y * (a[0] - c[0])) / delta;
 
-        return false; 
+        // Check if s and t are within the strict intersection range
+        const T max = T{1} - tol;
+        return (s > tol && s < max && t > tol && t < max);
     }
 
-    template <typename T>
+    template <std::floating_point T>
     bool on_segment(
         const std::array<T, 2> &a,
         const std::array<T, 2> &b,
@@ -160,7 +231,7 @@ namespace gbs
         return t >= T(0) && t <= T(1);
     }
 
-    template <typename T>
+    template <std::floating_point T>
     bool seg_D_strict_end_intersection(
         const std::array<T,2> &a,
         const std::array<T,2> &b,
@@ -200,7 +271,7 @@ namespace gbs
         return false; 
     }
 
-    template <typename T>
+    template <std::floating_point T>
     bool seg_H_strict_end_intersection(
         const std::array<T, 2> &a,
         const std::array<T, 2> &b,
--- tests/tests_baseIntersections.cpp
+++ tests/tests_baseIntersections.cpp
@@ -130,3 +130,97 @@ TEST(base_intersections, in_triangle)
         ASSERT_FALSE(in_triangle(a, b, c, d));
     }
 }
+
+TEST(base_intersections, in_triangle_barycentric)
+{
+    std::array<double, 2> a{1,1}, b{4,-2}, c{3,3};
+    { // On corner
+        ASSERT_TRUE(in_triangle_barycentric(a, b, c, a));
+        ASSERT_TRUE(in_triangle_barycentric(a, b, c, b));
+        ASSERT_TRUE(in_triangle_barycentric(a, b, c, c));
+    }
+    {// inside
+        std::array<double, 2> d{2., 1.};
+        ASSERT_TRUE(in_triangle_barycentric(a, b, c, d));
+    }
+
+    {// On side
+        std::array<double, 2> d{3., -1.}; 
+        ASSERT_TRUE(in_triangle_barycentric(a, b, c, d)); 
+    }
+
+    {// Outside
+        std::array<double, 2> d{1., -2.}; 
+        ASSERT_FALSE(in_triangle_barycentric(a, b, c, d));
+        d = {5,-4};
+        ASSERT_FALSE(in_triangle_barycentric(a, b, c, d));
+        d = {5,-1};
+        ASSERT_FALSE(in_triangle_barycentric(a, b, c, d));
+        d = {4,2};
+        ASSERT_FALSE(in_triangle_barycentric(a, b, c, d));
+        d = {1,3};
+        ASSERT_FALSE(in_triangle_barycentric(a, b, c, d));
+    }
+
+    {
+        auto generate_random_points = [](size_t num_points)
+        {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<double> dis(-10.0, 10.0);
+
+            std::vector<std::array<double, 2>> points(num_points);
+            for (auto &point : points)
+            {
+                point[0] = dis(gen);
+                point[1] = dis(gen);
+            }
+            return points;
+        };
+
+        size_t num_points = 100000;
+        size_t num_runs = 100;
+
+        std::array<double, 2> a{1.0, 1.0};
+        std::array<double, 2> b{3.0, 1.0};
+        std::array<double, 2> c{2.0, 3.0};
+
+        auto points = generate_random_points(num_points);
+
+        auto start_time = std::chrono::high_resolution_clock::now();
+        auto end_time = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed_time;
+
+        double in_triangle_time = 0;
+        double in_triangle_barycentric_time = 0;
+
+        for (size_t run = 0; run < num_runs; ++run)
+        {
+            start_time = std::chrono::high_resolution_clock::now();
+            for (const auto &point : points)
+            {
+                auto test = in_triangle<double>(a, b, c, point);
+            }
+            end_time = std::chrono::high_resolution_clock::now();
+            elapsed_time = end_time - start_time;
+            in_triangle_time += elapsed_time.count();
+
+            start_time = std::chrono::high_resolution_clock::now();
+            for (const auto &point : points)
+            {
+                auto test = in_triangle_barycentric<double>(a, b, c, point);
+            }
+            end_time = std::chrono::high_resolution_clock::now();
+            elapsed_time = end_time - start_time;
+            in_triangle_barycentric_time += elapsed_time.count();
+        }
+
+        std::cout << "in_triangle average time: "
+                  << in_triangle_time / num_runs
+                  << " seconds" << std::endl;
+
+        std::cout << "in_triangle_barycentric average time: "
+                  << in_triangle_barycentric_time / num_runs
+                  << " seconds" << std::endl;
+    }
+}
