0bfe5610f601b0673372017d3ba2ce632b4b0ce1
szebos <sebastien.newe@gmail.com>
2023-12-07 13:05:27 +0100

    Refactor test file path.

--- tests/tests_helpers.h
+++ tests/tests_helpers.h
@@ -8,3 +8,9 @@ auto measure_execution_time(F func, Args&&... args) {
     auto end_time = std::chrono::steady_clock::now();
     return std::chrono::duration_cast<T>(end_time - start_time).count();
 }
+
+// Function to extract the directory from a file path
+std::string get_directory(const std::string& file_path) {
+    size_t found = file_path.find_last_of("/\\");
+    return found != std::string::npos ? file_path.substr(0, found) : "";
+}
