dee20b02da057b09f0f9ac853701f948f237f908
szebos <seb.ssg.aero@gmail.com>
2026-04-19 15:44:12 +0200

    Fix ignored eval_msg parameter in OutOfBoundsEval
    The eval_msg constructor parameter was declared but never used,
    resulting in hardcoded "Eval " prefix for all subclasses
    (OutOfBoundsCurveEval, OutOfBoundsSurfaceUEval, OutOfBoundsSurfaceVEval,
    OutOfBoundsSurfaceIsoU, OutOfBoundsSurfaceIsoV), making thrown messages
    indistinguishable.

--- gbs/exceptions.h
+++ gbs/exceptions.h
@@ -8,8 +8,8 @@ namespace gbs{
     class OutOfBoundsEval : public std::domain_error
     {
     public:
-        explicit OutOfBoundsEval(T v, const std::array<T, 2> &bounds, const char *eval_msg ="Eval ") : 
-            std::domain_error{ "Eval "+  std::to_string(v) + " out of bounds [ " +  std::to_string(bounds[0]) + " , "  + std::to_string(bounds[1]) + " ] error."}
+        explicit OutOfBoundsEval(T v, const std::array<T, 2> &bounds, const char *eval_msg ="Eval ") :
+            std::domain_error{ std::string{eval_msg} +  std::to_string(v) + " out of bounds [ " +  std::to_string(bounds[0]) + " , "  + std::to_string(bounds[1]) + " ] error."}
             { }
 
     };
